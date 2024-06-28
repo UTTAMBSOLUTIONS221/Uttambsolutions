4e81b7599378daa78887582a669659b9a14e28bd
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-06-28 17:55:40 +0300

    update

---
+++ WEB/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using DBL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Controllers
+{
+    [Authorize]
+    public class CategoryController : BaseController
+    {
+        private readonly BL bl;
+        public CategoryController(IConfiguration config)
+        {
+            bl = new BL(Util.ShareConnectionString(config));
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
