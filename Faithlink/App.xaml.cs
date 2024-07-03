a689bd7e2e2f60f8dc27ea658793e7a511c21b3f
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-07-03 12:24:34 +0300

    upodate

---
+++ Faithlink/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Faithlink
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Faithlink/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Faithlink
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
