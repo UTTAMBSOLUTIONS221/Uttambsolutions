846a7c5101a63554c22fcbbbf84d5d92179ef9bb
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-07-12 10:34:40 +0300

    update

--- WEB/Controllers/SystemblogController.cs
+++ WEB/Controllers/SystemblogController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DBL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WEB.Controllers
 {
+    [Authorize]
     public class SystemblogController : Controller
     {
-        public IActionResult Index()
+        private readonly BL bl;
+        public SystemblogController(IConfiguration config)
+        {
+            bl = new BL(Util.ShareConnectionString(config));
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
             return View();
         }
