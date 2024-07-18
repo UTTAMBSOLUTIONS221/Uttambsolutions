bd88bc74b08393ecc3e0288ab6465833e4ba5cae
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-07-18 11:07:03 +0300

    update

--- WEB/Controllers/OpportunityController.cs
+++ WEB/Controllers/OpportunityController.cs
@@ -19,8 +19,8 @@ namespace WEB.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            //var data = await bl.Getsystemallopportuntydata(0, 1000);
-            return View();
+            var data = await bl.Getsystemallopportunitydata(0, 1000);
+            return View(data);
         }
         [HttpGet]
         public async Task<IActionResult> Addopportunity(int Opportunityid)
