fc91c113903392170cf62adea2fbd93cc8ce3209
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-07-18 11:05:10 +0300

    update

--- DBL/Entities/SystemJob.cs
+++ DBL/Entities/SystemJob.cs
@@ -4,19 +4,25 @@
     {
         public int JobId { get; set; }
         public int EmployerId { get; set; }
-        public string Title { get; set; }
-        public string JobDescription { get; set; }
+        public string? Employername { get; set; }
+        public string? Employerlogo { get; set; }
+        public string? Title { get; set; }
+        public string? JobDescription { get; set; }
         public int? JobFunctionId { get; set; }
+        public string? Functionname { get; set; }
         public int? JobIndustryId { get; set; }
+        public string? Jobindustryname { get; set; }
         public int? JobLocationId { get; set; }
+        public string? Locationname { get; set; }
         public int? JobTypeId { get; set; }
-        public string JobTypeName { get; set; }
+        public string? Jobtypename { get; set; }
         public int? JobExperienceId { get; set; }
-        public string JobSalaryRange { get; set; }
+        public string? Experiencename { get; set; }
+        public string? JobSalaryRange { get; set; }
         public DateTime? Deadline { get; set; }
-        public string JobStatus { get; set; } = "Open";
+        public string? JobStatus { get; set; } = "Open";
         public DateTime DatePosted { get; set; } = DateTime.Now;
-        public string JobUrl { get; set; }
+        public string? JobUrl { get; set; }
         public bool EasyApply { get; set; } = false;
         public bool HasTest { get; set; } = false;
         public bool Approved { get; set; } = false;
