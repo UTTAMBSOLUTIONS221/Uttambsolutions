2dab4504b01f9923542d17e1b9d67f8fddb2bd62
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-07-24 13:19:59 +0300

    update

--- DBL/Entities/SystemUserLog.cs
+++ DBL/Entities/SystemUserLog.cs
@@ -2,14 +2,14 @@
 {
     public class SystemUserLog
     {
-        public int LogId { get; set; }
-        public int UserId { get; set; }
-        public string LogAction { get; set; }
-        public string Browser { get; set; }
-        public string IpAddress { get; set; }
-        public decimal LoyaltyReward { get; set; }
-        public int LoyaltyStatus { get; set; }
-        public int LogActionExitTime { get; set; }
-        public DateTime DateCreated { get; set; }
+        public int Logid { get; set; }
+        public int Userid { get; set; }
+        public string? Logaction { get; set; }
+        public string? Browser { get; set; }
+        public string? Ipaddress { get; set; }
+        public decimal Loyaltyreward { get; set; }
+        public int Loyaltystatus { get; set; }
+        public int LogactionexitTime { get; set; }
+        public DateTime Datecreated { get; set; }
     }
 }
