1243d9dbe8da23428483483c66fe21186875af93
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-07-24 16:42:22 +0300

    update

--- DBL/Entities/SystemOrganization.cs
+++ DBL/Entities/SystemOrganization.cs
@@ -6,6 +6,7 @@
         public long OrganizationOwner { get; set; }
         public string? OrganizationName { get; set; }
         public string? OrganizationDescription { get; set; }
+        public string? OrganizationLogo { get; set; }
         public int OrganizationTypeId { get; set; }
         public int OrganizationStatus { get; set; }
         public string? OrganizationPhone { get; set; }
