9f731225a1f4937bd9379121ba56a6a10d599335
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-07-28 12:54:17 +0300

    update

---
+++ API/Controllers/EcommerceController.cs
@@ -0,0 +1,24 @@
+﻿using DBL;
+using DBL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EcommerceController : ControllerBase
+    {
+        private readonly BL bl;
+        IConfiguration _config;
+        public EcommerceController(IConfiguration config)
+        {
+            bl = new BL(Util.ShareConnectionString(config));
+            _config = config;
+        }
+        [HttpGet("Getsystemorganizationshopproductsdata")]
+        public async Task<Systemorganizationshopproducts> Getsystemorganizationshopproductsdata()
+        {
+            return await bl.Getsystemorganizationshopproductsdata();
+        }
+    }
+}
