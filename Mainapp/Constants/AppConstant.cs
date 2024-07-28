0d1249c7f8f566868be9034b242b1b2c67924ce6
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-07-28 10:59:35 +0300

    update

--- Mainapp/Constants/AppConstant.cs
+++ Mainapp/Constants/AppConstant.cs
@@ -10,60 +10,87 @@ namespace Mainapp.Constants
         {
             AppShell.Current.FlyoutHeader = new FlyoutHeaderControl();
 
-            // Remove previous flyout items
             var itemsToRemove = AppShell.Current.Items.Where(f => f.Route == nameof(CommonDashboardPage) || f.Route == nameof(SokojijiDashboardPage)).ToList();
             foreach (var item in itemsToRemove)
             {
                 AppShell.Current.Items.Remove(item);
             }
 
-            // Add new flyout items based on the current page
-            var flyoutItem = new FlyoutItem()
-            {
-                Title = $"{pageName} Menu",
-                Route = pageName,
-                FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
-                Items =
-                {
-                    new ShellContent
-                    {
-                        Icon = Icons.Dashboard,
-                        Title = "Dashboard",
-                        ContentTemplate = new DataTemplate(typeof(CommonDashboardPage)),
-                    },
-                    new ShellContent
-                    {
-                        Icon = Icons.People,
-                        Title = "Bible",
-                        ContentTemplate = new DataTemplate(typeof(SokojijiDashboardPage)),
-                    },
-                    new ShellContent
-                    {
-                        Icon = Icons.People,
-                        Title = "Forums",
-                        ContentTemplate = new DataTemplate(typeof(SokojijiDashboardPage)),
-                    },
-                    new ShellContent
-                    {
-                        Icon = Icons.People,
-                        Title = "Groups",
-                        ContentTemplate = new DataTemplate(typeof(SokojijiDashboardPage)),
-                    },
-                    new ShellContent
-                    {
-                        Icon = Icons.AboutUs,
-                        Title = "Profile",
-                        ContentTemplate = new DataTemplate(typeof(SokojijiDashboardPage)),
-                    },
-                }
-            };
+            FlyoutItem flyoutItem = null;
 
-            if (!AppShell.Current.Items.Contains(flyoutItem))
+            if (pageName == nameof(SokojijiDashboardPage))
+            {
+                flyoutItem = new FlyoutItem()
+                {
+                    Title = "Sokojiji Dashboard",
+                    Route = pageName,
+                    FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Icon = Icons.Dashboard,
+                            Title = "Dashboard",
+                            ContentTemplate = new DataTemplate(typeof(SokojijiDashboardPage)),
+                        },
+                        new ShellContent
+                        {
+                            Icon = Icons.People,
+                            Title = "Bible",
+                            ContentTemplate = new DataTemplate(typeof(SokojijiDashboardPage)),
+                        },
+                        new ShellContent
+                        {
+                            Icon = Icons.People,
+                            Title = "Forums",
+                            ContentTemplate = new DataTemplate(typeof(SokojijiDashboardPage)),
+                        },
+                        new ShellContent
+                        {
+                            Icon = Icons.People,
+                            Title = "Groups",
+                            ContentTemplate = new DataTemplate(typeof(SokojijiDashboardPage)),
+                        },
+                        new ShellContent
+                        {
+                            Icon = Icons.AboutUs,
+                            Title = "Profile",
+                            ContentTemplate = new DataTemplate(typeof(SokojijiDashboardPage)),
+                        },
+                    }
+                };
+            }
+            else if (pageName == nameof(CommonDashboardPage))
+            {
+                flyoutItem = new FlyoutItem()
+                {
+                    Title = "Common Dashboard",
+                    Route = pageName,
+                    FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Icon = Icons.Dashboard,
+                            Title = "Dashboard",
+                            ContentTemplate = new DataTemplate(typeof(CommonDashboardPage)),
+                        },
+                        new ShellContent
+                        {
+                            Icon = Icons.People,
+                            Title = "Profile",
+                            ContentTemplate = new DataTemplate(typeof(CommonDashboardPage)),
+                        },
+                    }
+                };
+            }
+
+            if (flyoutItem != null && !AppShell.Current.Items.Contains(flyoutItem))
             {
                 AppShell.Current.Items.Add(flyoutItem);
             }
 
-            // Navigate to the new page using absolute routing
+            // Navigate to the requested page
             await Shell.Current.GoToAsync($"///{pageName}");
         }
     }
