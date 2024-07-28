73f0d57b322c52f1eba60ab6374b3007c619ea57
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-07-28 12:54:57 +0300

    update

--- API/Controllers/EcommerceController.cs
+++ API/Controllers/EcommerceController.cs
@@ -1,11 +1,13 @@
 ﻿using DBL;
 using DBL.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class EcommerceController : ControllerBase
     {
         private readonly BL bl;
@@ -15,6 +17,7 @@ namespace API.Controllers
             bl = new BL(Util.ShareConnectionString(config));
             _config = config;
         }
+        [AllowAnonymous]
         [HttpGet("Getsystemorganizationshopproductsdata")]
         public async Task<Systemorganizationshopproducts> Getsystemorganizationshopproductsdata()
         {
