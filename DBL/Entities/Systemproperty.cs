95100303def907a861a008a02edd1ca8a8f4e82b
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-07-31 13:52:40 +0300

    update

--- DBL/Entities/Systemproperty.cs
+++ DBL/Entities/Systemproperty.cs
@@ -6,10 +6,11 @@
         public long Propertyowner { get; set; }
         public long Propertyposter { get; set; }
         public string? Propertyname { get; set; }
-        public string? Streetorlandmark { get; set; }
         public long Countyid { get; set; }
         public long Subcountyid { get; set; }
         public long Subcountywardid { get; set; }
+        public string? Streetorlandmark { get; set; }
+        public bool Hasdeposit { get; set; }
         public long Createdby { get; set; }
         public long Modifiedby { get; set; }
         public DateTime Datecreated { get; set; }
