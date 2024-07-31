47bb33c951b43bf3533f7699444a403a751ce67b
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-07-31 08:56:39 +0300

    update

---
+++ DBL/Entities/Systemproperty.cs
@@ -0,0 +1,14 @@
+﻿namespace DBL.Entities
+{
+    public class Systemproperty
+    {
+        public long Propertyid { get; set; }
+        public long Propertyowner { get; set; }
+        public long Propertyposter { get; set; }
+        public string? Propertyname { get; set; }
+        public long Createdby { get; set; }
+        public long Modifiedby { get; set; }
+        public DateTime Datecreated { get; set; }
+        public DateTime Datemodified { get; set; }
+    }
+}
---
+++ WEB/Controllers/PropertyController.cs
@@ -0,0 +1,27 @@
+﻿using DBL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Controllers
+{
+
+    [Authorize]
+    public class PropertyController : BaseController
+    {
+        private readonly BL bl;
+        public PropertyController(IConfiguration config)
+        {
+            bl = new BL(Util.ShareConnectionString(config));
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Addproperty()
+        {
+            return PartialView();
+        }
+    }
+}
