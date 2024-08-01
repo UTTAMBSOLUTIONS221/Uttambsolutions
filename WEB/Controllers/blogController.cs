a89e411ce61dbbb91bf0f3445a01af6329bf3d36
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-01 08:56:32 +0300

    update

---
+++ WEB/Controllers/blogController.cs
@@ -0,0 +1,21 @@
+﻿using DBL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Controllers
+{
+    [Authorize]
+    public class blogController : BaseController
+    {
+        private readonly BL bl;
+        public blogController(IConfiguration config)
+        {
+            bl = new BL(Util.ShareConnectionString(config));
+        }
+        public async Task<IActionResult> Index()
+        {
+            var blogs = await bl.Getsystemallblogdata(0, 10000);
+            return View(blogs);
+        }
+    }
+}
