4c2fa1b4b3250c2b1168c80970f59bd36d501d69
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-02 05:31:46 +0300

    update

--- Maqaoplus/Platforms/Android/MainActivity.cs
+++ Maqaoplus/Platforms/Android/MainActivity.cs
@@ -7,5 +7,16 @@ namespace Maqaoplus
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set the status bar color
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                var window = Platform.CurrentActivity.Window; // Get the current activity's window
+                window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#0a506c")); // Change this to your desired color
+            }
+        }
     }
 }
