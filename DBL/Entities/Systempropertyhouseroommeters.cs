65c1dba681488a821787768236c9733c306871da
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-03 09:40:14 +0300

    update

--- DBL/Entities/Systempropertyhouseroommeters.cs
+++ DBL/Entities/Systempropertyhouseroommeters.cs
@@ -5,11 +5,22 @@
         public int Systempropertyhousemeterid { get; set; }
         public int Systempropertyhouseroomid { get; set; }
         public string? Systempropertyhouseroommeternumber { get; set; }
-        public decimal Openinmeter { get; set; }
+        public decimal Openingmeter { get; set; }
+        public decimal Movedmeter { get; set; }
+        public decimal Closingmeter { get; set; }
+        public int Createdby { get; set; }
+        public DateTime Datecreated { get; set; }
+        public List<Systempropertyhouseroommeterhistory>? Data { get; set; }
+    }
+    public class Systempropertyhouseroommeterhistory
+    {
+        public int Systempropertyhousemeterid { get; set; }
+        public int Systempropertyhouseroomid { get; set; }
+        public string? Systempropertyhouseroommeternumber { get; set; }
+        public decimal Openingmeter { get; set; }
         public decimal Movedmeter { get; set; }
         public decimal Closingmeter { get; set; }
         public int Createdby { get; set; }
         public DateTime Datecreated { get; set; }
     }
-
 }
