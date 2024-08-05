3a44b442a53b8c9043930beb6f6d6f2a9e96119c
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-05 17:56:44 +0300

    update

--- DBL/Entities/Verifyaccountmodel.cs
+++ DBL/Entities/Verifyaccountmodel.cs
@@ -5,5 +5,6 @@
         public long Userid { get; set; }
         public long Accountnumber { get; set; }
         public string? Phonenumber { get; set; }
+        public string? Newphonenumber { get; set; }
     }
 }
