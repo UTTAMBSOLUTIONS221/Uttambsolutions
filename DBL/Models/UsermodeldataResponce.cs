9cf7ad1c2d211a6ad1141bbeb6073b6e14b61af6
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-05 12:03:26 +0300

    update

--- DBL/Models/UsermodeldataResponce.cs
+++ DBL/Models/UsermodeldataResponce.cs
@@ -2,31 +2,36 @@
 {
     public class UsermodeldataResponce
     {
-        public long Userid { get; set; }
-        public long Tenantid { get; set; }
-        public string? Tenantname { get; set; }
-        public string? Tenantsubdomain { get; set; }
-        public string? UserProfileImageUrl { get; set; }
-        public string? Usercurriculumvitae { get; set; }
-        public string? Currencyname { get; set; }
-        public string? Utcname { get; set; }
+        public int Userid { get; set; }
         public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
         public string? Fullname { get; set; }
         public string? Phonenumber { get; set; }
         public string? Username { get; set; }
         public string? Emailaddress { get; set; }
+        public int Genderid { get; set; }
+        public int Maritalstatusid { get; set; }
         public int Roleid { get; set; }
-        public string? Rolename { get; set; }
         public string? Passharsh { get; set; }
         public string? Passwords { get; set; }
         public bool Isactive { get; set; }
         public bool Isdeleted { get; set; }
-        public int Loginstatus { get; set; }
-        public DateTime Passwordresetdate { get; set; }
-        public long Createdby { get; set; }
-        public long Modifiedby { get; set; }
-        public DateTime Lastlogin { get; set; }
-        public DateTime Datemodified { get; set; }
+        public bool Isdefault { get; set; }
+        public bool Loginstatus { get; set; }
+        public DateTime? Passwordresetdate { get; set; }
+        public int? Parentid { get; set; }
+        public string? Userprofileimageurl { get; set; }
+        public string? Usercurriculumvitae { get; set; }
+        public string? Idnumber { get; set; }
+        public bool Updateprofile { get; set; }
+        public decimal Walletbalance { get; set; } = 0;
+        public string? Rolename { get; set; }
+        public string? RoleDescription { get; set; }
+        public int Tenantid { get; set; }
+        public int Createdby { get; set; }
+        public int Modifiedby { get; set; }
+        public DateTime? Lastlogin { get; set; }
+        public DateTime? Datemodified { get; set; }
         public DateTime Datecreated { get; set; }
     }
 }
