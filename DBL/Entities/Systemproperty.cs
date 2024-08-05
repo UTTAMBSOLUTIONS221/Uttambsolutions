c2fe1ca74b817d53062031200d6adfde942d1385
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-05 10:48:42 +0300

    update

--- DBL/Entities/Systemproperty.cs
+++ DBL/Entities/Systemproperty.cs
@@ -24,7 +24,7 @@
         public int Watertypeid { get; set; }
         public decimal Waterunitprice { get; set; }
         public decimal Monthlybinfee { get; set; }
-        public int Rentduedate { get; set; }
+        public int Rentdueday { get; set; }
         public string? Propertyhousestatusdata { get; set; }
         public string? Extra { get; set; }
         public string? Extra1 { get; set; }
