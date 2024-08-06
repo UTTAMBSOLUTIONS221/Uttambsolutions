22afeaad843ffb55a328e24b6ca0683520eb2522
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-06 12:45:15 +0300

    update

---
+++ Maqaoplus/Helpers/BooleanToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Maqaoplus.Helpers
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool booleanValue)
+            {
+                return booleanValue ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
