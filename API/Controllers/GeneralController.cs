2c3f4ebfa068c44e6b1006a751eb9eca86376402
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-07 17:26:58 +0300

    update

---
+++ API/Controllers/GeneralController.cs
@@ -0,0 +1,33 @@
+﻿using DBL;
+using DBL.Enum;
+using DBL.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class GeneralController : ControllerBase
+    {
+        private readonly BL bl;
+        IConfiguration _config;
+        public GeneralController(IConfiguration config)
+        {
+            bl = new BL(Util.ShareConnectionString(config));
+            _config = config;
+        }
+        [HttpGet]
+        public List<ListModel> Systemdropdowns(ListModelType listType)
+        {
+            return bl.GetListModel(listType).Result.Select(x => new ListModel
+            {
+                Text = x.Text,
+                Value = x.Value,
+                GroupId = x.GroupId,
+                GroupName = x.GroupName,
+            }).ToList();
+        }
+    }
+}
