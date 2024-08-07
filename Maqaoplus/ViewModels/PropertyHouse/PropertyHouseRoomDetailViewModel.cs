08509ca21cb05a2657862e1c895c9e515bb7796c
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-07 11:19:42 +0300

    update

--- Maqaoplus/ViewModels/PropertyHouse/PropertyHouseRoomDetailViewModel.cs
+++ Maqaoplus/ViewModels/PropertyHouse/PropertyHouseRoomDetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using DBL.Entities;
 using Newtonsoft.Json;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace Maqaoplus.ViewModels.PropertyHouse
@@ -9,18 +10,52 @@ namespace Maqaoplus.ViewModels.PropertyHouse
         private readonly Services.ServiceProvider _serviceProvider;
         private long _propertyRoomId;
         private Systempropertyhouserooms _houseroomData;
-        public ICommand LoadRoomDetailCommand { get; }
-
         private bool _isLoading;
-        public bool IsLoading
+        private ObservableCollection<Systempropertykitchentype> _systemkitchentype;
+        private Systempropertykitchentype _selectedKitchentype;
+        private ObservableCollection<Systempropertyhousesize> _systempropertyhousesize;
+        private Systempropertyhousesize _selectedSize;
+        private string _systempropertyhousesizename;
+        private bool _continueWithoutTenant;
+        private bool _forcaretaker;
+        private bool _isshop;
+        private bool _isgroundfloor;
+        private bool _hasbalcony;
+        private bool _isunderrenovation;
+        private ObservableCollection<MeterReading> _meterReadings;
+        private ObservableCollection<AllSystemData> _allSystemData;
+        private string _searchId;
+        private string _searchResults;
+
+        public ICommand LoadRoomDetailCommand { get; }
+        public ICommand SaveCommand { get; }
+        public ICommand SearchCommand { get; }
+        public ICommand CloseCommand { get; }
+
+        public PropertyHouseRoomDetailViewModel()
         {
-            get => _isLoading;
+            LoadRoomDetailCommand = new Command(async () => await LoadRoomDetails());
+            SaveCommand = new Command(async () => await SaveRoomDetails());
+            SearchCommand = new Command(async () => await Search());
+            CloseCommand = new Command(() => Close());
+        }
+
+        public PropertyHouseRoomDetailViewModel(Services.ServiceProvider serviceProvider) : this()
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public long PropertyRoomId
+        {
+            get => _propertyRoomId;
             set
             {
-                _isLoading = value;
+                _propertyRoomId = value;
                 OnPropertyChanged();
+                LoadRoomDetailCommand.Execute(null);
             }
         }
+
         public Systempropertyhouserooms HouseroomData
         {
             get => _houseroomData;
@@ -31,22 +66,164 @@ namespace Maqaoplus.ViewModels.PropertyHouse
             }
         }
 
-        // Parameterless constructor
-        public PropertyHouseRoomDetailViewModel()
+        public bool IsLoading
         {
-            LoadRoomDetailCommand = new Command(async () => await LoadRoomDetails());
+            get => _isLoading;
+            set
+            {
+                _isLoading = value;
+                OnPropertyChanged();
+            }
         }
 
-        // Constructor with parameter
-        public PropertyHouseRoomDetailViewModel(Services.ServiceProvider serviceProvider) : this()
+        public ObservableCollection<Systempropertykitchentype> Systemkitchentype
         {
-            _serviceProvider = serviceProvider;
+            get => _systemkitchentype;
+            set
+            {
+                _systemkitchentype = value;
+                OnPropertyChanged();
+            }
         }
 
-        public void SetPropertyRoomId(long propertyRoomId)
+        public Systempropertykitchentype SelectedKitchentype
         {
-            _propertyRoomId = propertyRoomId;
-            LoadRoomDetailCommand.Execute(null);
+            get => _selectedKitchentype;
+            set
+            {
+                _selectedKitchentype = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Systempropertyhousesize> Systempropertyhousesize
+        {
+            get => _systempropertyhousesize;
+            set
+            {
+                _systempropertyhousesize = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Systempropertyhousesize SelectedSize
+        {
+            get => _selectedSize;
+            set
+            {
+                _selectedSize = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Systempropertyhousesizename
+        {
+            get => _systempropertyhousesizename;
+            set
+            {
+                _systempropertyhousesizename = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool ContinueWithoutTenant
+        {
+            get => _continueWithoutTenant;
+            set
+            {
+                _continueWithoutTenant = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Forcaretaker
+        {
+            get => _forcaretaker;
+            set
+            {
+                _forcaretaker = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Isshop
+        {
+            get => _isshop;
+            set
+            {
+                _isshop = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Isgroundfloor
+        {
+            get => _isgroundfloor;
+            set
+            {
+                _isgroundfloor = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Hasbalcony
+        {
+            get => _hasbalcony;
+            set
+            {
+                _hasbalcony = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Isunderrenovation
+        {
+            get => _isunderrenovation;
+            set
+            {
+                _isunderrenovation = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<MeterReading> MeterReadings
+        {
+            get => _meterReadings;
+            set
+            {
+                _meterReadings = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<AllSystemData> AllSystemData
+        {
+            get => _allSystemData;
+            set
+            {
+                _allSystemData = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SearchId
+        {
+            get => _searchId;
+            set
+            {
+                _searchId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SearchResults
+        {
+            get => _searchResults;
+            set
+            {
+                _searchResults = value;
+                OnPropertyChanged();
+            }
         }
 
         private async Task LoadRoomDetails()
@@ -55,10 +232,15 @@ namespace Maqaoplus.ViewModels.PropertyHouse
 
             try
             {
-                var response = await _serviceProvider.CallAuthWebApi<object>("/api/PropertyHouse/Getsystempropertyhousedetaildatabypropertyidandownerid/" + _propertyRoomId, HttpMethod.Get, null);
+                var response = await _serviceProvider.CallAuthWebApi<object>(
+                    $"/api/PropertyHouse/Getsystempropertyhousedetaildatabypropertyidandownerid/{_propertyRoomId}",
+                    HttpMethod.Get, null
+                );
+
                 if (response != null)
                 {
                     HouseroomData = JsonConvert.DeserializeObject<Systempropertyhouserooms>(response.Data.ToString());
+                    // Populate other properties if needed
                 }
             }
             catch (Exception ex)
@@ -69,6 +251,43 @@ namespace Maqaoplus.ViewModels.PropertyHouse
             {
                 IsLoading = false;
             }
+        }
+
+        private async Task SaveRoomDetails()
+        {
+            IsLoading = true;
+
+            try
+            {
+                var response = await _serviceProvider.CallAuthWebApi<object>(
+                    "/api/PropertyHouse/SaveRoomDetails",
+                    HttpMethod.Post,
+                    JsonConvert.SerializeObject(HouseroomData)
+                );
+
+                if (response != null)
+                {
+                    // Handle response and show success message if needed
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+
+        private async Task Search()
+        {
+            // Implement search logic based on SearchId
+        }
+
+        private void Close()
+        {
+            // Implement close logic, if needed
         }
     }
 }
