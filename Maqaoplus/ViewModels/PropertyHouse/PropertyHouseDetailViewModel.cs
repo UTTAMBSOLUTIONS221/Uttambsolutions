0279edbb7dac5d5fe591b0974b05e0b92c6e484e
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-07 08:35:32 +0300

    update

--- Maqaoplus/ViewModels/PropertyHouse/PropertyHouseDetailViewModel.cs
+++ Maqaoplus/ViewModels/PropertyHouse/PropertyHouseDetailViewModel.cs
@@ -1,25 +1,65 @@
-﻿using DBL.Entities;
+﻿using DBL.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Maqaoplus.ViewModels.PropertyHouse
 {
-
     public class PropertyHouseDetailViewModel : BaseViewModel
     {
-        private Systemproperty _property;
+        private readonly Services.ServiceProvider _serviceProvider;
+        private long _propertyId;
 
-        public Systemproperty Property
+        public ObservableCollection<PropertyHouseDetails> Rooms { get; }
+        public ICommand LoadRoomsCommand { get; }
+
+        private bool _isLoading;
+        public bool IsLoading
         {
-            get => _property;
-            set => SetProperty(ref _property, value);
+            get => _isLoading;
+            set
+            {
+                _isLoading = value;
+                OnPropertyChanged();
+            }
         }
 
-        public PropertyHouseDetailViewModel()
+        public PropertyHouseDetailViewModel(Services.ServiceProvider serviceProvider)
         {
+            _serviceProvider = serviceProvider;
+            Rooms = new ObservableCollection<PropertyHouseDetails>();
+            LoadRoomsCommand = new Command(async () => await LoadRooms());
         }
 
-        public PropertyHouseDetailViewModel(Systemproperty property)
+        public void SetPropertyId(long propertyId)
         {
-            Property = property;
+            _propertyId = propertyId;
+            LoadRoomsCommand.Execute(null);
+        }
+
+        private async Task LoadRooms()
+        {
+            IsLoading = true;
+
+            try
+            {
+                var response = await _serviceProvider.CallAuthWebApi<object>("/api/PropertyHouse/Getsystempropertyhousetenantdatabytenantid/" + App.UserDetails.Usermodel.Userid, HttpMethod.Get, null);
+                if (response != null && response.Data != null)
+                {
+                    Rooms.Clear();
+                    foreach (var room in response.Data)
+                    {
+                        Rooms.Add(room);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+            }
+            finally
+            {
+                IsLoading = false;
+            }
         }
     }
 }
--- Maqaoplus/Views/PropertyHouse/PropertyHousesDetailPage.xaml.cs
+++ Maqaoplus/Views/PropertyHouse/PropertyHousesDetailPage.xaml.cs
@@ -1,19 +1,26 @@
+using Maqaoplus.ViewModels.PropertyHouse;
 namespace Maqaoplus.Views.PropertyHouse
 {
     [QueryProperty(nameof(PropertyId), "PropertyId")]
+
     public partial class PropertyHousesDetailPage : ContentPage
     {
         public long PropertyId { get; set; }
-
-        public PropertyHousesDetailPage()
+        private PropertyHouseDetailViewModel _viewModel;
+        public PropertyHousesDetailPage(PropertyHouseDetailViewModel viewModel)
         {
             InitializeComponent();
+            BindingContext = viewModel;
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
 
+            if (BindingContext is PropertyHouseDetailViewModel viewModel)
+            {
+                viewModel.SetPropertyId(PropertyId);
+            }
             // Use PropertyId to load details or set up the page
             System.Diagnostics.Debug.WriteLine($"Property ID received: {PropertyId}");
         }
