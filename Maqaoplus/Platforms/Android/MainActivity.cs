bbfa7f31783166bd4e89b5490bb5efb19917d410
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-09 20:30:52 +0300

    update

--- Maqaoplus/Platforms/Android/MainActivity.cs
+++ Maqaoplus/Platforms/Android/MainActivity.cs
@@ -1,10 +1,9 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
-
 namespace Maqaoplus
 {
-    [Activity(MainLauncher = true, Theme = "@style/AppTheme", LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
