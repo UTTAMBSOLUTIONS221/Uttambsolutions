5c775ed513f2ae22f8e9aaa16d7c294a7c881348
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-11 09:09:48 +0300

    update

--- Maqaoplus/ViewModels/PropertyHouse/AddPropertyHouseViewModel.cs
+++ Maqaoplus/ViewModels/PropertyHouse/AddPropertyHouseViewModel.cs
@@ -1,24 +1,126 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace Maqaoplus.ViewModels.PropertyHouse
 {
     public class AddPropertyHouseViewModel : INotifyPropertyChanged
     {
-        private string _firstName;
-        private string _lastName;
-        private string _emailAddress;
-        private string _phoneNumber;
-        private string _password;
-        private string _confirmPassword;
+
+
+
+        private bool _isStep1Visible;
+        private bool _isStep2Visible;
+        private bool _isStep3Visible;
+        private bool _isStep4Visible;
+
         private bool _isProcessing;
 
         public event PropertyChangedEventHandler PropertyChanged;
         private readonly Services.ServiceProvider _serviceProvider;
 
+
+        public ICommand NextCommand { get; }
+        public ICommand PreviousCommand { get; }
         public AddPropertyHouseViewModel(Services.ServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
+
+            NextCommand = new Command(NextStep);
+            PreviousCommand = new Command(PreviousStep);
+
+            // Initialize steps
+            _isStep1Visible = true;
+            _isStep2Visible = false;
+            _isStep3Visible = false;
+            _isStep4Visible = false;
+        }
+
+        public bool IsStep1Visible
+        {
+            get => _isStep1Visible;
+            set
+            {
+                _isStep1Visible = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsStep2Visible
+        {
+            get => _isStep2Visible;
+            set
+            {
+                _isStep2Visible = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsStep3Visible
+        {
+            get => _isStep3Visible;
+            set
+            {
+                _isStep3Visible = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsStep4Visible
+        {
+            get => _isStep4Visible;
+            set
+            {
+                _isStep4Visible = value;
+                OnPropertyChanged();
+            }
+        }
+        private void NextStep()
+        {
+            // Move to the next step
+            if (_isStep1Visible)
+            {
+                _isStep1Visible = false;
+                _isStep2Visible = true;
+            }
+            else if (_isStep2Visible)
+            {
+                _isStep2Visible = false;
+                _isStep3Visible = true;
+            }
+            else if (_isStep3Visible)
+            {
+                _isStep3Visible = false;
+                _isStep4Visible = true;
+            }
+            OnPropertyChanged(nameof(IsStep1Visible));
+            OnPropertyChanged(nameof(IsStep2Visible));
+            OnPropertyChanged(nameof(IsStep3Visible));
+            OnPropertyChanged(nameof(IsStep4Visible));
+        }
+
+        private void PreviousStep()
+        {
+            // Move to the previous step
+            if (_isStep4Visible)
+            {
+                _isStep4Visible = false;
+                _isStep3Visible = true;
+            }
+            else if (_isStep3Visible)
+            {
+                _isStep3Visible = false;
+                _isStep2Visible = true;
+            }
+            else if (_isStep2Visible)
+            {
+                _isStep2Visible = false;
+                _isStep1Visible = true;
+            }
+            OnPropertyChanged(nameof(IsStep1Visible));
+            OnPropertyChanged(nameof(IsStep2Visible));
+            OnPropertyChanged(nameof(IsStep3Visible));
+            OnPropertyChanged(nameof(IsStep4Visible));
         }
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
