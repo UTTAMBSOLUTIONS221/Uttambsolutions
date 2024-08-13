85479f5a58f85e7173ad266738a8941777badd89
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-13 11:03:39 +0300

    update

---
+++ DBL/Models/PropertyHouseTenantData.cs
@@ -0,0 +1,18 @@
+﻿namespace DBL.Models
+{
+    public class PropertyHouseTenantData
+    {
+        public PropertyHouseTenant? Data { get; set; }
+    }
+    public class PropertyHouseTenant
+    {
+        public int Systempropertyhousetenantid { get; set; }
+        public string? Tenantname { get; set; }
+        public string? Propertyhousename { get; set; }
+        public string? Propertyprimaryimage { get; set; }
+        public string? Systempropertyhousesizename { get; set; }
+        public bool Isoccupant { get; set; }
+        public DateTime Datecreated { get; set; }
+        public DateTime Datemodified { get; set; }
+    }
+}
