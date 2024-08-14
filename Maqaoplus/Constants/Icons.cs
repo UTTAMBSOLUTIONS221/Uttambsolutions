fa5908c8aabb0fd8e4dcccda2ee36a4c172d3e12
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-14 13:27:55 +0300

    update

--- Maqaoplus/Constants/Icons.cs
+++ Maqaoplus/Constants/Icons.cs
@@ -5,8 +5,8 @@
         public const string Dashboard = "dashboard.png";
         public const string user = "user.png";
         public const string house = "house.png";
-        public const string reports = "group-users.png";
-        public const string groupusers = "reports.png";
+        public const string reports = "reports.png";
+        public const string groupusers = "groupusers.png";
 
         public const string ContactUs = "contact_us.png";
         public const string AboutUs = "about_us.png";
