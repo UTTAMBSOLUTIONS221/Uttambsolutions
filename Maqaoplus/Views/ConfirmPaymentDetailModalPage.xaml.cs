895eba595b8ce6d28f17a9b1ec5fa6ab452b33c1
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-14 06:42:57 +0300

    update

--- Maqaoplus/Views/ConfirmPaymentDetailModalPage.xaml.cs
+++ Maqaoplus/Views/ConfirmPaymentDetailModalPage.xaml.cs
@@ -5,21 +5,18 @@ namespace Maqaoplus.Views;
 
 public partial class ConfirmPaymentDetailModalPage : ContentPage
 {
-    public partial class ConfirmPaymentDetailModalPage : ContentPage
+    public ICommand OkCommand { get; }
+    public ICommand CancelCommand { get; }
+
+    public ConfirmPaymentDetailModalPage(SystemStaff staffData, ICommand okCommand, ICommand cancelCommand)
     {
-        public ICommand OkCommand { get; }
-        public ICommand CancelCommand { get; }
+        InitializeComponent();
+        BindingContext = staffData;
+        OkCommand = okCommand;
+        CancelCommand = cancelCommand;
 
-        public ConfirmPaymentDetailModalPage(SystemStaff staffData, ICommand okCommand, ICommand cancelCommand)
-        {
-            InitializeComponent();
-            BindingContext = staffData;
-            OkCommand = okCommand;
-            CancelCommand = cancelCommand;
-
-            // Bind the commands to the buttons explicitly
-            okButton.Command = OkCommand;
-            cancelButton.Command = CancelCommand;
-        }
+        // Bind the commands to the buttons explicitly
+        okButton.Command = OkCommand;
+        cancelButton.Command = CancelCommand;
     }
 }
