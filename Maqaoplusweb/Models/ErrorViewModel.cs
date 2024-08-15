fc739a7295d01760f1f580d8c195159ffb1570aa
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-15 14:11:09 +0300

    update

---
+++ Maqaoplusweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Maqaoplusweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
