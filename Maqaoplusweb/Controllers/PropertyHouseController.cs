6d5bab9cbfc8d51c81f3d047413afdc9fb7d3096
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-15 16:26:41 +0300

    update

---
+++ Maqaoplusweb/Controllers/PropertyHouseController.cs
@@ -0,0 +1,20 @@
+﻿using DBL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Maqaoplusweb.Controllers
+{
+    [Authorize]
+    public class PropertyHouseController : BaseController
+    {
+        private readonly BL bl;
+        public PropertyHouseController(IConfiguration config)
+        {
+            bl = new BL(Util.ShareConnectionString(config));
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
