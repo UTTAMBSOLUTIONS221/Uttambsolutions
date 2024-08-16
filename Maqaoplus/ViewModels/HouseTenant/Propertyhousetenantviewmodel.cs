cd09485ff889b225e40985c1dc1e247c8b34392a
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-16 20:48:25 +0300

    update

--- Maqaoplus/ViewModels/HouseTenant/Propertyhousetenantviewmodel.cs
+++ Maqaoplus/ViewModels/HouseTenant/Propertyhousetenantviewmodel.cs
@@ -101,8 +101,6 @@ namespace Maqaoplus.ViewModels.HouseTenant
             }
         }
 
-
-
         private async Task NeedtoVacatethisHouseAsync()
         {
             IsVacatingProcessing = true;
