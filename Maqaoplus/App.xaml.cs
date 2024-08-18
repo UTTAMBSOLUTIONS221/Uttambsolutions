b588b399fdf0c2fca1e30dd73b4c95310df891a1
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-18 16:01:01 +0300

    update

--- Maqaoplus/App.xaml.cs
+++ Maqaoplus/App.xaml.cs
@@ -9,6 +9,15 @@ namespace Maqaoplus
             InitializeComponent();
 
             MainPage = appShell;
+            CheckConnectivity();
+        }
+        private void CheckConnectivity()
+        {
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            {
+                // Handle no internet connection
+                MainPage.DisplayAlert("No Internet", "You are not connected to the internet.", "OK");
+            }
         }
     }
 }
