7497921841cbe9178ac4e2d36e5b23faa626535d
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-21 08:45:23 +0300

    update

--- Maqaoplus/Views/OwnerBillsandPayments/PropertyHousesOwnerBillsPage.xaml.cs
+++ Maqaoplus/Views/OwnerBillsandPayments/PropertyHousesOwnerBillsPage.xaml.cs
@@ -1,9 +1,23 @@
+using Maqaoplus.ViewModels.OwnerBillsandPayments;
 namespace Maqaoplus.Views.OwnerBillsandPayments;
-
 public partial class PropertyHousesOwnerBillsPage : ContentPage
 {
-	public PropertyHousesOwnerBillsPage()
-	{
-		InitializeComponent();
-	}
+    private PropertyHousesOwnerBillsViewModel _viewModel;
+
+    public PropertyHousesOwnerBillsPage(Services.ServiceProvider serviceProvider)
+    {
+        InitializeComponent();
+        _viewModel = new PropertyHousesOwnerBillsViewModel(serviceProvider);
+        this.BindingContext = _viewModel;
+
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (_viewModel.LoadItemsCommand.CanExecute(null))
+        {
+            _viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
 }
