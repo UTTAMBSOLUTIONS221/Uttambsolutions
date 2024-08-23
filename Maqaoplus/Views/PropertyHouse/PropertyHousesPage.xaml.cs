48a2a78531407bcaf6969dafd39b53b7221fe12a
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-23 20:20:01 +0300

    update

--- Maqaoplus/Views/PropertyHouse/PropertyHousesPage.xaml.cs
+++ Maqaoplus/Views/PropertyHouse/PropertyHousesPage.xaml.cs
@@ -1,5 +1,4 @@
 using Maqaoplus.ViewModels.PropertyHouse;
-using System.Collections.Specialized;
 namespace Maqaoplus.Views.PropertyHouse;
 
 public partial class PropertyHousesPage : ContentPage
@@ -11,11 +10,6 @@ public partial class PropertyHousesPage : ContentPage
         InitializeComponent();
         _viewModel = new PropertyHouseViewModel(serviceProvider);
         this.BindingContext = _viewModel;
-        // Subscribe to the CollectionChanged event
-        _viewModel.Items.CollectionChanged += OnItemsCollectionChanged;
-
-        // Set default layout
-        PropertyHousesCollectionView.ItemsLayout = new GridItemsLayout(2, ItemsLayoutOrientation.Vertical);
 
     }
 
@@ -26,27 +20,5 @@ public partial class PropertyHousesPage : ContentPage
         {
             _viewModel.LoadItemsCommand.Execute(null);
         }
-    }
-    private void OnItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-    {
-        if (PropertyHousesCollectionView.ItemsSource != null)
-        {
-            var itemCount = _viewModel.Items.Count;
-            var span = CalculateSpan(itemCount);
-            PropertyHousesCollectionView.ItemsLayout = new GridItemsLayout(span, ItemsLayoutOrientation.Vertical);
-        }
-    }
-    private int CalculateSpan(int itemCount)
-    {
-        if (itemCount == 1)
-            return 2; // Fill the page row
-
-        if (itemCount == 2 || itemCount == 4)
-            return 2; // Divide by two
-
-        if (itemCount == 3 || itemCount == 5)
-            return 3; // Three on top and two below or similar
-
-        return 2; // Default span
     }
 }
