3b11cf8a93b6cc2b5f415d0a62574ff68edc8d43
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-25 13:14:52 +0300

    update

---
+++ DBL/Models/OwnerTenantAgreementDetailDataModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DBL.Models
+{
+    public class OwnerTenantAgreementDetailDataModel
+    {
+        public OwnerTenantAgreementDetailData Data { get; set; }
+    }
+    public class OwnerTenantAgreementDetailData
+    {
+        public int Propertyhouseid { get; set; }
+        public string? Propertyhouseowner { get; set; }
+        public string? Fullname { get; set; }
+        public string? Phonenumber { get; set; }
+        public string? Emailaddress { get; set; }
+        public string? Countyname { get; set; }
+        public string? Subcountyname { get; set; }
+        public string? Subcountywardname { get; set; }
+        public DateTime OwnerDatecreated { get; set; }
+        public DateTime TenantDatecreated { get; set; }
+        public string? OwnerSignatureimageurl { get; set; }
+        public string? TenantSignatureimageurl { get; set; }
+    }
+
+}
