c4999e458abb4fcf6ef327d6aaa98904f091a280
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-26 10:59:23 +0300

    update

---
+++ DBL/Entities/SystemPropertyHouseImage.cs
@@ -0,0 +1,11 @@
+﻿namespace DBL.Entities
+{
+    public class SystemPropertyHouseImage
+    {
+        public long PropertyImageId { get; set; }
+        public long PropertyHouseId { get; set; }
+        public string? HouseOrRoom { get; set; }
+        public string? SignatureImageUrl { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
