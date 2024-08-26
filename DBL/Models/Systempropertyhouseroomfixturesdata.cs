9030ad8699565ca4a06163bd36996baa30d9090e
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-26 15:11:08 +0300

    update

---
+++ DBL/Models/Systempropertyhouseroomfixturesdata.cs
@@ -0,0 +1,27 @@
+﻿namespace DBL.Models
+{
+    public class Systempropertyhouseroomfixturesdata
+    {
+        public Systempropertyhouseroomfixtures? Data { get; set; }
+    }
+    public class Systempropertyhouseroomfixtures
+    {
+        public int Systempropertyhouseroomid { get; set; }
+        public int Systempropertyhouseid { get; set; }
+        public List<RoomFixture>? Roomfixtures { get; set; }
+    }
+    public class RoomFixture
+    {
+        public int Propertychecklistid { get; set; }
+        public int Propertyhouseroomid { get; set; }
+        public int Fixturestatusid { get; set; }
+        public string? Fixturestatus { get; set; }
+        public int Createdby { get; set; }
+        public DateTime Datecreated { get; set; }
+        public int Fixtureid { get; set; }
+        public string? Fixturetype { get; set; }
+        public string? Descriptions { get; set; }
+        public string? Category { get; set; }
+    }
+
+}
