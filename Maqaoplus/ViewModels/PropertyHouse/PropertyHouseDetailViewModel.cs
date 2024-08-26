c3682de94cae15add821e693f7a2dd320340b992
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-26 12:38:03 +0300

    update

--- Maqaoplus/ViewModels/PropertyHouse/PropertyHouseDetailViewModel.cs
+++ Maqaoplus/ViewModels/PropertyHouse/PropertyHouseDetailViewModel.cs
@@ -331,7 +331,7 @@ namespace Maqaoplus.ViewModels.PropertyHouse
             var response = await _serviceProvider.CallAuthWebApi<object>("/api/PropertyHouse/Getsystempropertyhouseroomimagebyhouseroomid/" + propertyHouseRoomId, HttpMethod.Get, null);
             if (response != null)
             {
-                SystemPropertyHouseImageData = JsonConvert.DeserializeObject<SystemPropertyHouseImageData>(response.Data.ToString());
+                SystemPropertyHouseImageData = JsonConvert.DeserializeObject<SystemPropertyHouseImage>(response.Data.ToString());
             }
             var modalPage = new SystemPropertyHouseRoomImagesModalPage(this);
             await Application.Current.MainPage.Navigation.PushModalAsync(modalPage);
