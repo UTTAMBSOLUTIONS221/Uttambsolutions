c999b4a1bdc5406f5d46e40eda75340858b8cda1
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-27 18:11:27 +0300

    update

--- Maqaoplus/ViewModels/PropertyHouseTenantAgreement/PropertyHouseTenantAgreementViewModel.cs
+++ Maqaoplus/ViewModels/PropertyHouseTenantAgreement/PropertyHouseTenantAgreementViewModel.cs
@@ -122,6 +122,8 @@ namespace Maqaoplus.ViewModels.PropertyHouseTenantAgreement
                 IsProcessing = false;
             }
         }
+
+
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
