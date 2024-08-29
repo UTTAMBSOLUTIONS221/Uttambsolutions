24a99e97ab069a84e25d29f7cef21b3598d06eb2
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-29 14:13:51 +0300

    update

--- DBL/Entities/CustomerPaymentValidation.cs
+++ DBL/Entities/CustomerPaymentValidation.cs
@@ -2,12 +2,26 @@
 {
     public class CustomerPaymentValidation
     {
-        public long CustomerPaymentId { get; set; }
-        public long Houseroomid { get; set; }
-        public long Financetransactionid { get; set; }
+        public int CustomerPaymentId { get; set; }
+        public int HouseRoomTenantId { get; set; }
+        public int Houseroomid { get; set; }
+        public int PaymentModeId { get; set; }
+        public int Financetransactionid { get; set; }
         public long Tenantid { get; set; }
         public long Confirmedby { get; set; }
+        public decimal Amount { get; set; }
         public decimal Actualamount { get; set; }
+        public string? TransactionReference { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public bool IsPaymentValidated { get; set; }
+        public string? ChequeNo { get; set; }
+        public DateTime? ChequeDate { get; set; }
+        public string? Memo { get; set; }
+        public string? DrawerBank { get; set; }
+        public string? DepositBank { get; set; }
+        public int PaidBy { get; set; }
+        public int ValidatedBy { get; set; }
+        public string? SlipReference { get; set; }
         public DateTime Datecreated { get; set; }
         public DateTime Datemodified { get; set; }
     }
