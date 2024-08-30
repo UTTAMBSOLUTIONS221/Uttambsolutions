59ed65567005b6af5591df0a72ff022fe1820ced
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-30 20:11:01 +0300

    update

--- Maqaoplus/Views/Dashboards/AgentDashboardPage.xaml.cs
+++ Maqaoplus/Views/Dashboards/AgentDashboardPage.xaml.cs
@@ -1,9 +1,21 @@
+using Maqaoplus.ViewModels.Dashboards;
+
 namespace Maqaoplus.Views.Dashboards;
 
 public partial class AgentDashboardPage : ContentPage
 {
-	public AgentDashboardPage()
-	{
-		InitializeComponent();
-	}
+    public AgentDashboardPage(SummaryDashBoardViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        if (BindingContext is SummaryDashBoardViewModel viewModel && viewModel.LoadItemsCommand.CanExecute(null))
+        {
+            viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
 }
--- Maqaoplus/Views/Dashboards/CaretakerDashboardPage.xaml.cs
+++ Maqaoplus/Views/Dashboards/CaretakerDashboardPage.xaml.cs
@@ -1,9 +1,21 @@
+using Maqaoplus.ViewModels.Dashboards;
+
 namespace Maqaoplus.Views.Dashboards;
 
 public partial class CaretakerDashboardPage : ContentPage
 {
-	public CaretakerDashboardPage()
-	{
-		InitializeComponent();
-	}
+    public CaretakerDashboardPage(SummaryDashBoardViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        if (BindingContext is SummaryDashBoardViewModel viewModel && viewModel.LoadItemsCommand.CanExecute(null))
+        {
+            viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
 }
