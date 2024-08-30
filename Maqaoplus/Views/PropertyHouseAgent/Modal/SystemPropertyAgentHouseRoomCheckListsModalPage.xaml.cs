ed566711318b5edffd353321078b5b188409a464
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-08-30 19:21:57 +0300

    update

---
+++ Maqaoplus/Views/PropertyHouseAgent/Modal/SystemPropertyAgentHouseRoomCheckListsModalPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Maqaoplus.Views.PropertyHouseAgent.Modal;
+
+public partial class SystemPropertyAgentHouseRoomCheckListsModalPage : ContentPage
+{
+	public SystemPropertyAgentHouseRoomCheckListsModalPage()
+	{
+		InitializeComponent();
+	}
+}
