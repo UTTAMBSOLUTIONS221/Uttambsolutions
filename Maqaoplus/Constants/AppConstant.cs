db04c0475474bfe5b8a23ad95414b9cc5b290259
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-09-02 13:59:42 +0300

    update

--- Maqaoplus/Constants/AppConstant.cs
+++ Maqaoplus/Constants/AppConstant.cs
@@ -2,7 +2,6 @@
 using Maqaoplus.Views;
 using Maqaoplus.Views.BillsandPayments;
 using Maqaoplus.Views.Dashboards;
-using Maqaoplus.Views.OwnerBillsandPayments;
 using Maqaoplus.Views.PropertyHouse;
 using Maqaoplus.Views.PropertyHouseTenantAgreement;
 using Maqaoplus.Views.PropertyHouseTenants;
@@ -70,13 +69,13 @@ namespace Maqaoplus.Constants
                         {
                             Icon = Icons.invoice,
                             Title = "Bills",
-                            ContentTemplate = new DataTemplate(() => new PropertyHousesOwnerBillsPage(serviceProvider)),
+                            ContentTemplate = new DataTemplate(() => new OwnerPropertyHousesBillsPage(serviceProvider)),
                         },
                             new ShellContent
                         {
                             Icon = Icons.dollar,
                             Title = "Payments",
-                            ContentTemplate = new DataTemplate(() => new PropertyHousesOwnerPaymentsPage(serviceProvider)),
+                            ContentTemplate = new DataTemplate(() => new OwnerPropertyHousesPaymentsPage(serviceProvider)),
                         },
                     }
                 };
