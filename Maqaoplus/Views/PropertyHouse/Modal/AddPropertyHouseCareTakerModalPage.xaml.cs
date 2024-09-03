8aa7383428f81698a88714a5d38719e314e6b2b3
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-09-03 09:11:06 +0300

    update

---
+++ Maqaoplus/Views/PropertyHouse/Modal/AddPropertyHouseCareTakerModalPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Maqaoplus.Views.PropertyHouse.Modal;
+
+public partial class AddPropertyHouseCareTakerModalPage : ContentPage
+{
+	public AddPropertyHouseCareTakerModalPage()
+	{
+		InitializeComponent();
+	}
+}
