bb23316827522a2602412f8a592ecd2cbd980f8f
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-09-03 08:46:04 +0300

    update

--- Maqaoplus/Views/PropertyHouse/PropertyHousesCaretakerPage.xaml.cs
+++ Maqaoplus/Views/PropertyHouse/PropertyHousesCaretakerPage.xaml.cs
@@ -1,9 +1,25 @@
+using Maqaoplus.ViewModels.PropertyHouse;
+
 namespace Maqaoplus.Views.PropertyHouse;
 
 public partial class PropertyHousesCaretakerPage : ContentPage
 {
-	public PropertyHousesCaretakerPage()
-	{
-		InitializeComponent();
-	}
+    private PropertyHouseViewModel _viewModel;
+
+    public PropertyHousesCaretakerPage(Services.ServiceProvider serviceProvider)
+    {
+        InitializeComponent();
+        _viewModel = new PropertyHouseViewModel(serviceProvider);
+        this.BindingContext = _viewModel;
+
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (_viewModel.LoadItemsCommand.CanExecute(null))
+        {
+            _viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
 }
