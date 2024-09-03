869297c0580c201fd44abc53e38c07532336afd5
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-09-03 09:12:26 +0300

    update

--- Maqaoplus/Views/PropertyHouse/Modal/AddPropertyHouseCareTakerModalPage.xaml.cs
+++ Maqaoplus/Views/PropertyHouse/Modal/AddPropertyHouseCareTakerModalPage.xaml.cs
@@ -1,9 +1,12 @@
+using Maqaoplus.ViewModels.PropertyHouse;
+
 namespace Maqaoplus.Views.PropertyHouse.Modal;
 
 public partial class AddPropertyHouseCareTakerModalPage : ContentPage
 {
-	public AddPropertyHouseCareTakerModalPage()
-	{
-		InitializeComponent();
-	}
+    public AddPropertyHouseCareTakerModalPage(PropertyHouseViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
 }
