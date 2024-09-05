fb44509227ef9df82ad7024caf4e283d8a13ba95
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-09-05 09:56:51 +0300

    update

--- Maqaoplus/ViewModels/PropertyHouse/PropertyHouseViewModel.cs
+++ Maqaoplus/ViewModels/PropertyHouse/PropertyHouseViewModel.cs
@@ -2,6 +2,7 @@
 using DBL.Enum;
 using DBL.Models;
 using Maqaoplus.Views;
+using Maqaoplus.Views.PropertyHouse;
 using Maqaoplus.Views.PropertyHouse.Modal;
 using Newtonsoft.Json;
 using System.Collections.ObjectModel;
@@ -27,7 +28,13 @@ namespace Maqaoplus.ViewModels.PropertyHouse
         private SystemPropertyHouseImage _systemPropertyHouseImageData;
         private Systemtenantdetails _tenantStaffData;
         private SystemStaff _systemstaffdata;
+        private Systemtenantdetails _newTenantStaffData;
+        private Systempropertyhouseroomfixtures _systempropertyhouseroomfixturesData;
 
+        private decimal _openingMeter;
+        private decimal _closingMeter;
+        private decimal _movedMeter;
+        private decimal _consumedAmount;
 
         private bool _isStep1Visible;
         private bool _isStep2Visible;
@@ -39,6 +46,7 @@ namespace Maqaoplus.ViewModels.PropertyHouse
         public ICommand AddAgentPropertyHouseCommand { get; }
         public ICommand AddPropertyHouseCareTakerCommand { get; }
         public ICommand LoadItemsCommand { get; }
+
         public ICommand LoadPropertyHouseCaretakerItemsCommand { get; }
         public ICommand LoadVacantPropertyHousesCommand { get; }
         public ICommand RefreshCommand { get; }
@@ -56,6 +64,13 @@ namespace Maqaoplus.ViewModels.PropertyHouse
         public ICommand SearchCommand { get; }
         public ICommand SearchStaffsCommand { get; }
         public ICommand SavePropertyHouseCareTakerCommand { get; }
+
+
+        public ICommand ViewRoomDetailsCommand { get; }
+        public ICommand ViewPropertyRoomCheckListCommand { get; }
+        public ICommand ViewPropertyRoomImageCommand { get; }
+        public ICommand SavePropertyHouseRoomFixtureCommand { get; }
+
 
         private bool _isLoading;
         public bool IsLoading
@@ -183,7 +198,6 @@ namespace Maqaoplus.ViewModels.PropertyHouse
             return !string.IsNullOrEmpty(rentingTerms) && rentingTerms.Contains("Fixedterm", StringComparison.OrdinalIgnoreCase);
         }
 
-
         public OwnerTenantAgreementDetailData OwnerTenantAgreementDetailData
         {
             get => _ownerTenantAgreementDetailData;
@@ -228,6 +242,80 @@ namespace Maqaoplus.ViewModels.PropertyHouse
                 OnPropertyChanged();
             }
         }
+
+        public Systempropertyhouserooms HouseroomData
+        {
+            get => _houseroomData;
+            set
+            {
+                _houseroomData = value;
+                OnPropertyChanged();
+            }
+        }
+        public Systemtenantdetails NewTenantStaffData
+        {
+            get => _newTenantStaffData;
+            set
+            {
+                _newTenantStaffData = value;
+                OnPropertyChanged();
+            }
+        }
+        public Systempropertyhouseroomfixtures SystempropertyhouseroomfixturesData
+        {
+            get => _systempropertyhouseroomfixturesData;
+            set
+            {
+                _systempropertyhouseroomfixturesData = value;
+                OnPropertyChanged();
+            }
+        }
+        private ObservableCollection<ListModel> _systemkitchentype;
+        public ObservableCollection<ListModel> Systemkitchentype
+        {
+            get => _systemkitchentype;
+            set
+            {
+                _systemkitchentype = value;
+                OnPropertyChanged();
+            }
+        }
+        private ListModel _selectedKitchentype;
+        public ListModel SelectedKitchentype
+        {
+            get => _selectedKitchentype;
+            set
+            {
+                _selectedKitchentype = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<ListModel> _systempropertyhousesize;
+        public ObservableCollection<ListModel> Systempropertyhousesize
+        {
+            get => _systempropertyhousesize;
+            set
+            {
+                _systempropertyhousesize = value;
+                OnPropertyChanged();
+            }
+        }
+        private ListModel _selectedPropertyhousesize;
+        public ListModel SelectedPropertyhousesize
+        {
+            get => _selectedPropertyhousesize;
+            set
+            {
+                if (_selectedPropertyhousesize != value)
+                {
+                    _selectedPropertyhousesize = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
         // Error properties
         private string _propertyHouseNameError;
         public string PropertyHouseNameError
@@ -449,6 +537,146 @@ namespace Maqaoplus.ViewModels.PropertyHouse
             }
         }
 
+        private string _propertyHouseRoomNumberError;
+        public string PropertyHouseRoomNumberError
+        {
+            get => _propertyHouseRoomNumberError;
+            set
+            {
+                _propertyHouseRoomNumberError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _propertyHouseRoomRentError;
+        public string PropertyHouseRoomRentError
+        {
+            get => _propertyHouseRoomRentError;
+            set
+            {
+                _propertyHouseRoomRentError = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _propertyHouseRoomoccupantError;
+        public string PropertyHouseRoomoccupantError
+        {
+            get => _propertyHouseRoomoccupantError;
+            set
+            {
+                _propertyHouseRoomoccupantError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _propertyHouseRoomoccupantdetailError;
+        public string PropertyHouseRoomoccupantdetailError
+        {
+            get => _propertyHouseRoomoccupantdetailError;
+            set
+            {
+                _propertyHouseRoomoccupantdetailError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _propertyHouseKitchenTypeError;
+        public string PropertyHouseKitchenTypeError
+        {
+            get => _propertyHouseKitchenTypeError;
+            set
+            {
+                _propertyHouseKitchenTypeError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _propertyHouseSizeError;
+        public string PropertyHouseSizeError
+        {
+            get => _propertyHouseSizeError;
+            set
+            {
+                _propertyHouseSizeError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _propertyHouseRoomClosingMeterError;
+        public string PropertyHouseRoomClosingMeterError
+        {
+            get => _propertyHouseRoomClosingMeterError;
+            set
+            {
+                _propertyHouseRoomClosingMeterError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _propertyHouseRoomTenantidError;
+        public string PropertyHouseRoomTenantidError
+        {
+            get => _propertyHouseRoomTenantidError;
+            set
+            {
+                _propertyHouseRoomTenantidError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public decimal OpeningMeter
+        {
+            get => _openingMeter;
+            set
+            {
+                _openingMeter = value;
+                OnPropertyChanged();
+                CalculateMeterValues();
+            }
+        }
+        public decimal ClosingMeter
+        {
+            get => _closingMeter;
+            set
+            {
+                _closingMeter = value;
+                OnPropertyChanged();
+                if (ClosingMeter > 0)
+                {
+                    CalculateMeterValues();
+                }
+            }
+        }
+
+        public decimal MovedMeter
+        {
+            get => _movedMeter;
+            set
+            {
+                _movedMeter = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public decimal ConsumedAmount
+        {
+            get => _consumedAmount;
+            set
+            {
+                _consumedAmount = value;
+                OnPropertyChanged();
+            }
+        }
+        private void CalculateMeterValues()
+        {
+            if (HouseroomData.Openingmeter >= 0 && ClosingMeter > 0)
+            {
+                MovedMeter = ClosingMeter - HouseroomData.Openingmeter;
+                ConsumedAmount = MovedMeter * HouseroomData.Waterunitprice;
+            }
+        }
+
+
         private string _careTakerFullname;
         public string CareTakerFullname
         {
@@ -467,6 +695,7 @@ namespace Maqaoplus.ViewModels.PropertyHouse
         public PropertyHouseViewModel(Services.ServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
+            LoadDropdownData();
             Items = new ObservableCollection<Systemproperty>();
             Rooms = new ObservableCollection<PropertyHouseDetails>();
             PropertyHouseCareTakerItems = new ObservableCollection<SystemStaff>();
@@ -493,7 +722,14 @@ namespace Maqaoplus.ViewModels.PropertyHouse
             SaveAgentPropertyHouseCommand = new Command(async () => await SaveAgentPropertyHouseAsync());
             SavePropertyHouseCareTakerCommand = new Command(async () => await SavePropertyHouseCareTakerAsync());
 
-            LoadDropdownData();
+            ViewRoomDetailsCommand = new Command<PropertyHouseDetails>(async (propertyRoom) => await ViewRoomDetails(propertyRoom.Systempropertyhouseroomid));
+            ViewPropertyRoomCheckListCommand = new Command<PropertyHouseDetails>(async (propertyRoom) => await ViewPropertyRoomCheckListDetailAsync(propertyRoom.Systempropertyhouseroomid));
+            ViewPropertyRoomImageCommand = new Command<PropertyHouseDetails>(async (propertyRoom) => await ViewPropertyRoomImagesDetails(propertyRoom.Systempropertyhouseroomid));
+
+
+
+
+
             // Initialize steps
             _isStep1Visible = true;
             _isStep2Visible = false;
@@ -526,7 +762,6 @@ namespace Maqaoplus.ViewModels.PropertyHouse
 
             _isLoadingMore = false;
         }
-
         public ObservableCollection<ListModel> Systemcounty
         {
             get => _systemcounty;
@@ -1352,7 +1587,7 @@ namespace Maqaoplus.ViewModels.PropertyHouse
                         var room = item.ToObject<PropertyHouseDetails>();
                         Rooms.Add(room);
                     }
-                    IsDataLoaded = true; // Data is loaded after processing
+                    IsDataLoaded = true;
                 }
                 var detailPage = new PropertyHousesDetailPage(this);
                 await Shell.Current.Navigation.PushAsync(detailPage);
@@ -1781,6 +2016,184 @@ namespace Maqaoplus.ViewModels.PropertyHouse
                 IsProcessing = false;
             }
         }
+
+
+        private async Task ViewRoomDetails(long propertyRoomId)
+        {
+            IsProcessing = true;
+            try
+            {
+                var response = await _serviceProvider.CallAuthWebApi<object>($"/api/PropertyHouse/Getsystempropertyhouseroomdatabyid/" + propertyRoomId, HttpMethod.Get, null);
+
+                if (response != null && response.Data != null)
+                {
+                    HouseroomData = JsonConvert.DeserializeObject<Systempropertyhouserooms>(response.Data.ToString());
+                    var kitchentypeResponse = await _serviceProvider.GetSystemDropDownData("/api/General?listType=" + ListModelType.Systemkitchentype, HttpMethod.Get);
+                    var sizeResponse = await _serviceProvider.GetSystemDropDownData("/api/General?listType=" + ListModelType.Systempropertyhousesizes, HttpMethod.Get);
+
+                    if (kitchentypeResponse != null)
+                    {
+                        Systemkitchentype = new ObservableCollection<ListModel>(kitchentypeResponse);
+                        SelectedKitchentype = Systemkitchentype.FirstOrDefault(x => x.Value == _houseroomData.Kitchentypeid.ToString());
+                    }
+                    if (sizeResponse != null)
+                    {
+                        Systempropertyhousesize = new ObservableCollection<ListModel>(sizeResponse);
+                        SelectedPropertyhousesize = Systempropertyhousesize.FirstOrDefault(x => x.Value == _houseroomData.Systempropertyhousesizeid.ToString());
+                    }
+
+                    var modalPage = new HousesRoomDetailModalPage(this);
+                    await Application.Current.MainPage.Navigation.PushModalAsync(modalPage);
+                    IsProcessing = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Navigation Error", ex.Message, "OK");
+            }
+            finally
+            {
+                IsProcessing = false; // Stop loading indicator
+            }
+        }
+        private async Task ViewPropertyRoomImagesDetails(long propertyHouseRoomId)
+        {
+            IsProcessing = true;
+            var response = await _serviceProvider.CallAuthWebApi<object>("/api/PropertyHouse/Getsystempropertyhouseroomimagebyhouseroomid/" + propertyHouseRoomId, HttpMethod.Get, null);
+            if (response != null)
+            {
+                SystemPropertyHouseImageData = JsonConvert.DeserializeObject<SystemPropertyHouseImage>(response.Data.ToString());
+            }
+            var modalPage = new SystemPropertyHouseRoomImagesModalPage(this);
+            await Application.Current.MainPage.Navigation.PushModalAsync(modalPage);
+            IsProcessing = false;
+        }
+        public async Task SavePropertyHouseRoomImageasync(string imageUrl)
+        {
+            IsProcessing = true;
+
+
+            if (SystemPropertyHouseImageData == null)
+            {
+                IsProcessing = false;
+                return;
+            }
+            SystemPropertyHouseImageData.Createdby = App.UserDetails.Usermodel.Userid;
+            SystemPropertyHouseImageData.Houseorroomimageurl = imageUrl;
+            SystemPropertyHouseImageData.Houseorroom = "HouseRoom";
+            SystemPropertyHouseImageData.Datecreated = DateTime.UtcNow;
+            try
+            {
+                var response = await _serviceProvider.CallCustomUnAuthWebApi("/api/PropertyHouse/Registersystempropertyhouseroomimagedata", SystemPropertyHouseImageData);
+                if (response.RespStatus == 200 || response.RespStatus == 0)
+                {
+                    Application.Current.MainPage.Navigation.PopModalAsync();
+                }
+                else if (response.RespStatus == 1)
+                {
+                    await Shell.Current.DisplayAlert("Warning", response.RespMessage, "OK");
+                }
+                else
+                {
+                    await Shell.Current.DisplayAlert("Error", "Sever error occured. Kindly Contact Admin!", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+            }
+            finally
+            {
+                IsProcessing = false;
+            }
+        }
+
+        private async Task ViewPropertyRoomCheckListDetailAsync(long propertyHouseRoomId)
+        {
+            IsProcessing = true;
+
+            // Fetch the room fixtures data
+            var response = await _serviceProvider.CallAuthWebApi<object>("/api/PropertyHouse/Getsystempropertyhouseroomfixturesdatabyhouseroomid/" + propertyHouseRoomId, HttpMethod.Get, null);
+            if (response != null)
+            {
+                SystempropertyhouseroomfixturesData = JsonConvert.DeserializeObject<Systempropertyhouseroomfixtures>(response.Data.ToString());
+            }
+
+            // Fetch the dropdown data
+            var systemPropertyFixturesResponse = await _serviceProvider.GetSystemDropDownData("/api/General?listType=" + ListModelType.Systempropertyfixtures, HttpMethod.Get);
+            if (systemPropertyFixturesResponse != null)
+            {
+                // Set SelectedFixture for each RoomFixture
+                foreach (var item in SystempropertyhouseroomfixturesData.Roomfixtures)
+                {
+                    item.Systempropertyfixturesdata = new ObservableCollection<ListModel>(systemPropertyFixturesResponse);
+                    item.SelectedFixture = item.Systempropertyfixturesdata.FirstOrDefault(x => x.Value == item.Fixturestatusid.ToString());
+                }
+            }
+
+            // Navigate to the modal page
+            var modalPage = new SystemPropertyHouseRoomCheckListsModalPage(this);
+            await Application.Current.MainPage.Navigation.PushModalAsync(modalPage);
+            IsProcessing = false;
+        }
+
+
+        public async Task SavePropertyHouseRoomFixtureasync()
+        {
+            IsProcessing = true;
+
+
+            if (SystempropertyhouseroomfixturesData == null)
+            {
+                IsProcessing = false;
+                return;
+            }
+            try
+            {
+                foreach (var fixture in SystempropertyhouseroomfixturesData.Roomfixtures)
+                {
+                    if (fixture.SelectedFixture != null)
+                    {
+                        // Set the Fixtureid to the selected value
+                        fixture.Fixturestatusid = int.Parse(fixture.SelectedFixture.Value);
+                    }
+                    if (fixture.Fixtureunits > 0 && fixture.Fixturestatusid <= 0)
+                    {
+                        await Shell.Current.DisplayAlert("Validation Error", "Fixture status is required when units are greater than 0.", "OK");
+                        IsProcessing = false;
+                        return;
+                    }
+                }
+                SystempropertyhouseroomfixturesData.Datecreated = DateTime.UtcNow;
+                SystempropertyhouseroomfixturesData.Createdby = App.UserDetails.Usermodel.Userid;
+                var response = await _serviceProvider.CallCustomUnAuthWebApi("/api/PropertyHouse/Registersystempropertyhouseroomfixturedata", SystempropertyhouseroomfixturesData);
+                if (response.RespStatus == 200 || response.RespStatus == 0)
+                {
+                    Application.Current.MainPage.Navigation.PopModalAsync();
+                }
+                else if (response.RespStatus == 1)
+                {
+                    await Shell.Current.DisplayAlert("Warning", response.RespMessage, "OK");
+                }
+                else
+                {
+                    await Shell.Current.DisplayAlert("Error", "Sever error occured. Kindly Contact Admin!", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+            }
+            finally
+            {
+                IsProcessing = false;
+            }
+        }
+
+
+
+
+
 
         private bool ValidateStep1()
         {
