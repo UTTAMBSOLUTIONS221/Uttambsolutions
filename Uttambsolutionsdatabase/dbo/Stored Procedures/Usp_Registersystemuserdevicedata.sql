0f512aa6a6cb0c12a55979130fb8f48b4e55a487
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-09-07 14:29:58 +0300

    update

--- Uttambsolutionsdatabase/dbo/Stored Procedures/Usp_Registersystemuserdevicedata.sql
+++ Uttambsolutionsdatabase/dbo/Stored Procedures/Usp_Registersystemuserdevicedata.sql
@@ -18,8 +18,8 @@ BEGIN
 			WITH (Systemuserdeviceid INT '$.Systemuserdeviceid',Userid BIGINT '$.Userid',Androidid VARCHAR(20) '$.Androidid',
 			Manufacturer VARCHAR(20) '$.Manufacturer',Model VARCHAR(20) '$.Model',Osversion VARCHAR(20) '$.Osversion',Platforms VARCHAR(20) '$.Platforms',Devicename VARCHAR(20) '$.Devicename',Datecreated DATETIME2 '$.Datecreated',Datemodified DATETIME2 '$.Datemodified'
 			)) AS source
-			ON target.Userid = source.Userid AND target.Androidid = source.Androidid
-			WHEN MATCHED THEN
+			ON target.Model = source.Model AND target.Androidid = source.Androidid 
+			WHEN MATCHED  AND source.Userid!=0 THEN
 			UPDATE SET target.Userid = source.Userid,target.Datemodified = source.Datemodified
 			WHEN NOT MATCHED BY TARGET THEN
 			INSERT (Userid,Androidid,Manufacturer,Model,Osversion,Platforms,Devicename,Datecreated,Datemodified)
---
+++ Uttambsolutionsdatabase/dbo/Stored Procedures/Usp_Verifyforgotpasswordsystemstaff.sql
@@ -0,0 +1,78 @@
+﻿CREATE PROCEDURE [dbo].[Usp_Verifyforgotpasswordsystemstaff]
+	@JsonObjectData varchar(200),
+    @StaffDetails NVARCHAR(MAX) OUTPUT
+AS
+BEGIN
+   BEGIN
+	DECLARE 	       
+	        @Passwordstatus VARCHAR(40) = 'Passwordgotten',
+			@RespStat int = 0,
+			@RespMsg varchar(150) = 'login success';
+		  
+	BEGIN
+		BEGIN TRY	
+		--validate
+				IF NOT EXISTS(SELECT Androidid FROM Systemuserdevices WHERE Androidid=JSON_VALUE(@JsonObjectData, '$.Androidid'))
+				BEGIN
+					SET @StaffDetails = (SELECT 1 as RespStatus, 'Mobile phone do not exist. Contact Admin!' as RespMessage,@StaffDetails AS StaffDetails FOR JSON PATH, INCLUDE_NULL_VALUES,WITHOUT_ARRAY_WRAPPER);
+					Return
+				END
+				IF NOT EXISTS(SELECT Userid FROM SystemStaffs WHERE Username=JSON_VALUE(@JsonObjectData, '$.Emailaddress'))
+				Begin
+					--Select  1 as RespStatus, 'Invalid Emailaddress or User does not Exist!' as RespMessage,@StaffDetails AS StaffDetails
+					SET @StaffDetails = (SELECT 1 as RespStatus, 'Invalid Emailaddress or User does not Exist!' as RespMessage,@StaffDetails AS StaffDetails FOR JSON PATH, INCLUDE_NULL_VALUES,WITHOUT_ARRAY_WRAPPER);
+					Return
+				End
+
+				IF EXISTS(SELECT Userid FROM SystemStaffs WHERE Username=JSON_VALUE(@JsonObjectData, '$.Emailaddress') AND IsActive=0)
+				Begin
+					SET @StaffDetails = (Select  1 as RespStatus, 'Inactive Account Contact Admin!' as RespMessage,@StaffDetails AS StaffDetails FOR JSON PATH, INCLUDE_NULL_VALUES,WITHOUT_ARRAY_WRAPPER);
+					Return
+				End
+				IF EXISTS(SELECT Userid FROM SystemStaffs WHERE Username=JSON_VALUE(@JsonObjectData, '$.Emailaddress') AND IsDeleted=1)
+				Begin
+					SET @StaffDetails = (Select  1 as RespStatus, 'Account Deleted Contact Admin!' as RespMessage,@StaffDetails AS StaffDetails FOR JSON PATH, INCLUDE_NULL_VALUES,WITHOUT_ARRAY_WRAPPER);
+					Return
+				End
+				IF EXISTS(SELECT Userid FROM SystemStaffs WHERE Username=JSON_VALUE(@JsonObjectData, '$.Emailaddress') AND LoginStatus=4)
+				Begin
+					SET @StaffDetails = (Select  1 as RespStatus, 'Account Blocked Contact Admin!' as RespMessage,@StaffDetails AS StaffDetails FOR JSON PATH, INCLUDE_NULL_VALUES,WITHOUT_ARRAY_WRAPPER);
+					Return
+				End
+		BEGIN TRANSACTION;
+			IF(JSON_VALUE(@JsonObjectData, '$.Userid') >0)
+			BEGIN
+			UPDATE SystemStaffs SET Passwords = JSON_VALUE(@JsonObjectData, '$.Passharsh'),Passharsh = JSON_VALUE(@JsonObjectData, '$.Passharsh'), Passwordresetdate = GETDATE() WHERE Userid = JSON_VALUE(@JsonObjectData, '$.Userid');
+			SET @Passwordstatus = 'Passwordupdated';
+			END
+			SET @StaffDetails = (
+				SELECT
+				(SELECT Systemstaff.Userid,Systemstaff.Firstname,Systemstaff.Lastname,Systemstaff.Firstname +' '+ Systemstaff.Lastname AS Fullname,Systemstaff.Phonenumber,Systemstaff.Username,
+					Systemstaff.Emailaddress,JSON_VALUE(@JsonObjectData, '$.Androidid') AS Androidid,@Passwordstatus AS Passwordstatus,Systemstaff.Genderid ,Systemstaff.Maritalstatusid,Systemstaff.Roleid,Systemstaff.Isactive,Systemstaff.Isdeleted,Systemstaff.Isdefault,
+					Systemstaff.Loginstatus,ISNULL(Systemstaffdesignation.Staffdesignation,'System Admin') AS Designation,Systemstaff.Passwordresetdate,Systemstaff.Parentid,Systemstaff.Userprofileimageurl,Systemstaff.Usercurriculumvitae,Systemstaff.Idnumber,Systemstaff.Updateprofile,ISNULL(Systemstaffsaccount.Accountnumber,0) AS Accountnumber,0 AS Walletbalance,
+					Systemrole.Rolename,Systemrole.RoleDescription,Systemrole.Tenantid,Systemstaff.Createdby,Systemstaff.Modifiedby,Systemstaff.Lastlogin,Systemstaff.Datemodified,Systemstaff.Datecreated
+					FROM Systemstaffs Systemstaff
+					INNER JOIN Systemroles Systemrole ON Systemstaff.Roleid=Systemrole.Roleid
+					LEFT JOIN Systemstaffsaccount Systemstaffsaccount ON Systemstaff.Userid=Systemstaffsaccount.Userid
+					LEFT JOIN Systemstaffdesignations Systemstaffdesignation ON Systemstaff.Userid=Systemstaffdesignation.Systemstaffid
+				WHERE Systemstaff.Username = JSON_VALUE(@JsonObjectData, '$.Emailaddress')
+				FOR JSON PATH, INCLUDE_NULL_VALUES,WITHOUT_ARRAY_WRAPPER
+				) AS Data
+				FOR JSON PATH, INCLUDE_NULL_VALUES,WITHOUT_ARRAY_WRAPPER
+			);	Set @RespMsg ='Success'
+		Set @RespStat =0; 
+		COMMIT TRANSACTION;
+		Select @RespStat as RespStatus, @RespMsg as RespMessage,@StaffDetails AS StaffDetails;
+
+		END TRY
+		BEGIN CATCH
+		ROLLBACK TRANSACTION
+		PRINT ''
+		PRINT 'Error ' + error_message();
+		Select 2 as RespStatus, '0 - Error(s) Occurred' + error_message() as RespMessage
+		END CATCH
+		Select @RespStat as RespStatus, @RespMsg as RespMessage;
+		RETURN; 
+		END;
+	END
+END
