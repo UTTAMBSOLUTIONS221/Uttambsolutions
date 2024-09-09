752eed94d2c90b633fc66a06c3fac5299bb617a2
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-09-09 21:29:47 +0300

    update

--- DBL/Models/Monthlyrentinvoicedata.cs
+++ DBL/Models/Monthlyrentinvoicedata.cs
@@ -6,6 +6,36 @@
     }
     public class Monthlyrentinvoice
     {
-
+        public int InvoiceId { get; set; }
+        public string? InvoiceNo { get; set; }
+        public string? FullName { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? PropertyHouseName { get; set; }
+        public string? SystemPropertyHouseSizeName { get; set; }
+        public int PropertyHouseRoomId { get; set; }
+        public int PropertyHouseRoomTenantId { get; set; }
+        public int FinanceTransactionId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DueDate { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Discount { get; set; }
+        public bool IsPaid { get; set; }
+        public decimal PaidAmount { get; set; }
+        public decimal Balance { get; set; }
+        public bool IsSent { get; set; }
+        public string? PaidStatus { get; set; }
+        public List<InvoiceDetail>? InvoiceDetails { get; set; }
     }
+    public class InvoiceDetail
+    {
+        public int InvoiceItemId { get; set; }
+        public int InvoiceId { get; set; }
+        public int SystemPropertyHouseDepositFeeId { get; set; }
+        public string? HouseDepositFeeName { get; set; }
+        public int Units { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+    }
+
 }
