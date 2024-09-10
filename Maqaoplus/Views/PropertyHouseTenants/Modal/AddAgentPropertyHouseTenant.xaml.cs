6ffbbcd1be4357dfdb63b57d4fe6f62dd150083e
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-09-10 13:03:24 +0300

    update

---
+++ Maqaoplus/Views/PropertyHouseTenants/Modal/AddAgentPropertyHouseTenant.xaml.cs
@@ -0,0 +1,9 @@
+namespace Maqaoplus.Views.PropertyHouseTenants.Modal;
+
+public partial class AddAgentPropertyHouseTenant : ContentPage
+{
+	public AddAgentPropertyHouseTenant()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Maqaoplus/Views/PropertyHouseTenants/Modal/AddOwnerPropertyHouseTenant.xaml.cs
@@ -0,0 +1,9 @@
+namespace Maqaoplus.Views.PropertyHouseTenants.Modal;
+
+public partial class AddOwnerPropertyHouseTenant : ContentPage
+{
+	public AddOwnerPropertyHouseTenant()
+	{
+		InitializeComponent();
+	}
+}
