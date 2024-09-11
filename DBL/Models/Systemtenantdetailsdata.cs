9a356869fd04f75619d00f7da2bcd432a8230f0d
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-09-11 09:56:34 +0300

    update

--- DBL/Models/Systemtenantdetailsdata.cs
+++ DBL/Models/Systemtenantdetailsdata.cs
@@ -7,6 +7,8 @@
     public class Systemtenantdetails
     {
         public int Userid { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
         public string? Fullname { get; set; }
         public string? Phonenumber { get; set; }
         public string? Emailaddress { get; set; }
