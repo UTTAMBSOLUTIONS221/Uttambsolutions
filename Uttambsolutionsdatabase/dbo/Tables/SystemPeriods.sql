86b410424b9b639b9e1150702b02e4905f310215
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-09-16 15:03:47 +0300

    update

---
+++ Uttambsolutionsdatabase/dbo/Tables/SystemPeriods.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[SystemPeriods] (
+    [PeriodId]         BIGINT   IDENTITY (1, 1) NOT NULL,
+    [Lastdateinperiod] DATETIME NOT NULL,
+    PRIMARY KEY CLUSTERED ([PeriodId] ASC)
+);
+
