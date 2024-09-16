aa83ad00d1114e97e08c630e1e47c85977a5cc86
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-09-16 09:08:38 +0300

    update

--- DBL/Entities/SystemPropertyHouseImage.cs
+++ DBL/Entities/SystemPropertyHouseImage.cs
@@ -2,7 +2,12 @@
 {
     public class SystemPropertyHouseImage
     {
+        public long Propertyimageid { get; set; }
         public long Propertyhouseid { get; set; }
+        public string? Houseorroom { get; set; }
+        public string? Houseorroomimageurl { get; set; }
+        public long Createdby { get; set; }
+        public DateTime Datecreated { get; set; }
         public List<SystemPropertyHouseImageModel>? PropertyHouseImage { get; set; }
     }
     public class SystemPropertyHouseImageModel
