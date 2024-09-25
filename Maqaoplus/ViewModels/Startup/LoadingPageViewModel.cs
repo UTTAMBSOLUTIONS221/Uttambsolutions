56c32360827326691d61e091c15b24f7bdbc15ac
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-09-25 12:30:52 +0300

    update

--- Maqaoplus/ViewModels/Startup/LoadingPageViewModel.cs
+++ Maqaoplus/ViewModels/Startup/LoadingPageViewModel.cs
@@ -32,8 +32,25 @@ namespace Maqaoplus.ViewModels.Startup
             else
             {
                 var userInfo = JsonConvert.DeserializeObject<UsermodelResponce>(userDetailsStr);
-                App.UserDetails = userInfo;
-                await AppConstant.AddFlyoutMenusDetails();
+                if (userInfo.Usermodel.Loginstatus != 0)
+                {
+                    if (DeviceInfo.Platform == DevicePlatform.WinUI)
+                    {
+                        AppShell.Current.Dispatcher.Dispatch(async () =>
+                        {
+                            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+                        });
+                    }
+                    else
+                    {
+                        await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+                    }
+                }
+                else
+                {
+                    App.UserDetails = userInfo;
+                    await AppConstant.AddFlyoutMenusDetails();
+                }
             }
         }
     }
