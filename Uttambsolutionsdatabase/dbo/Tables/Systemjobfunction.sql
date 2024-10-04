11c973a48ee9d671d81429f3bea3944cf1ea3b36
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-04 10:24:54 +0300

    update

--- Uttambsolutionsdatabase/dbo/Tables/Systemjobfunction.sql
+++ Uttambsolutionsdatabase/dbo/Tables/Systemjobfunction.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Systemjobfunction] (
-    [Jobfunctionid] INT            IDENTITY (1, 1) NOT NULL,
-    [Jobid]         INT            NOT NULL,
-    [Jobfunction]   NVARCHAR (800) NOT NULL,
-    [Datecreated]   DATETIME       DEFAULT (getdate()) NULL,
+    [Jobfunctionid] INT             IDENTITY (1, 1) NOT NULL,
+    [Jobid]         INT             NOT NULL,
+    [Jobfunction]   NVARCHAR (2000) NOT NULL,
+    [Datecreated]   DATETIME        DEFAULT (getdate()) NULL,
     PRIMARY KEY CLUSTERED ([Jobfunctionid] ASC)
 );
 
--- Uttambsolutionsdatabase/dbo/Tables/Systemjobqualification.sql
+++ Uttambsolutionsdatabase/dbo/Tables/Systemjobqualification.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Systemjobqualification] (
-    [Jobqualificationid] INT            IDENTITY (1, 1) NOT NULL,
-    [Jobid]              INT            NOT NULL,
-    [Jobqualification]   NVARCHAR (200) NOT NULL,
-    [Datecreated]        DATETIME       DEFAULT (getdate()) NULL,
+    [Jobqualificationid] INT             IDENTITY (1, 1) NOT NULL,
+    [Jobid]              INT             NOT NULL,
+    [Jobqualification]   NVARCHAR (2000) NOT NULL,
+    [Datecreated]        DATETIME        DEFAULT (getdate()) NULL,
     PRIMARY KEY CLUSTERED ([Jobqualificationid] ASC)
 );
 
