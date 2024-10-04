93a12f9fce06c7a4b56420d1b3420e3eeb7dd161
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-04 17:18:28 +0300

    update

---
+++ Uttambsolutionsdatabase/dbo/Tables/Propertyhouselistingimages.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Propertyhouselistingimages] (
+    [Houselistingid]        INT           NOT NULL,
+    [Propertyhouseimageurl] VARCHAR (300) NOT NULL,
+    [Datecreated]           DATETIME      NOT NULL
+);
+
