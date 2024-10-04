2ca4e61bc3453c2977b884edc9f7e8ac1b98bdda
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-04 15:40:21 +0300

    update

--- DBL/Entities/Propertyhouselisting.cs
+++ DBL/Entities/Propertyhouselisting.cs
@@ -12,6 +12,12 @@
         public string? Descriptions { get; set; }
         public string? Imageurl { get; set; }
         public string? Contacts { get; set; }
+        public bool Isactive { get; set; }
+        public bool Isdeleted { get; set; }
+        public int Createdby { get; set; }
+        public int Modifiedby { get; set; }
+        public DateTime Datemodified { get; set; }
+        public DateTime Datecreated { get; set; }
         public List<Propertyhouseimagesdata>? Propertyhouseimages { get; set; }
     }
     public class Propertyhouseimagesdata
