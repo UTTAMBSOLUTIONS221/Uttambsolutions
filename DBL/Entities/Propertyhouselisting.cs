f7c1205a3775b44681c6c28adab7ea0b4082e0cc
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-04 14:40:35 +0300

    update

--- DBL/Entities/Propertyhouselisting.cs
+++ DBL/Entities/Propertyhouselisting.cs
@@ -5,11 +5,11 @@
         public int Houselistingid { get; set; }
         public string? Title { get; set; }
         public decimal Price { get; set; }
-        public string? Location { get; set; }
+        public string? Locations { get; set; }
         public bool Isforrent { get; set; }
         public int Bedrooms { get; set; }
         public int Bathrooms { get; set; }
-        public string? Description { get; set; }
+        public string? Descriptions { get; set; }
         public string? Imageurl { get; set; }
         public string? Contacts { get; set; }
         public List<Propertyhouseimagesdata>? Propertyhouseimages { get; set; }
