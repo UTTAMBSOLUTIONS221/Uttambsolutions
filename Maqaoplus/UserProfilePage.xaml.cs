46dd26ab7afc4a0b171bbf21c0c072199bdfc9bb
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-06 16:52:32 +0300

    update

--- Maqaoplus/UserProfilePage.xaml.cs
+++ Maqaoplus/UserProfilePage.xaml.cs
@@ -3,21 +3,18 @@ using Maqaoplus.ViewModels;
 namespace Maqaoplus.Views;
 public partial class UserProfilePage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    private UserProfilePageViewModel _viewModel;
+    public UserProfilePage(UserProfilePageViewModel viewModel)
     {
-        private UserProfilePageViewModel _viewModel;
-        public MainPage(UserProfilePageViewModel viewModel)
+        InitializeComponent();
+        BindingContext = _viewModel = viewModel;
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (_viewModel.LoadCurrentUserCommand.CanExecute(null))
         {
-            InitializeComponent();
-            BindingContext = _viewModel = viewModel;
-        }
-        protected override async void OnAppearing()
-        {
-            base.OnAppearing();
-            if (_viewModel.LoadCurrentUserCommand.CanExecute(null))
-            {
-                _viewModel.LoadCurrentUserCommand.Execute(null);
-            }
+            _viewModel.LoadCurrentUserCommand.Execute(null);
         }
     }
 }
