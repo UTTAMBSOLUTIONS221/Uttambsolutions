a0811814ae8320706cbb1100daa6917abae4bd4b
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-07 16:35:18 +0300

    update

--- Maqaoplus/Models/PushNotificationReceived.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommunityToolkit.Mvvm.Messaging.Messages;
-
-namespace Maqaoplus.Models
-{
-    public class PushNotificationReceived : ValueChangedMessage<string>
-    {
-        public PushNotificationReceived(string message) : base(message) { }
-    }
-}
