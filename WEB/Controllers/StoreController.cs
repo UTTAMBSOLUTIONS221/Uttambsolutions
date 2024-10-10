9ea47fb544d4e43a2ffdf4eef0033d5274a7dc5c
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-10 19:53:27 +0300

    update

---
+++ WEB/Controllers/StoreController.cs
@@ -0,0 +1,27 @@
+﻿using DBL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Controllers
+{
+    [Authorize]
+    public class StoreController : BaseController
+    {
+        private readonly BL bl;
+        private readonly IWebHostEnvironment _env;
+
+        public StoreController(IConfiguration config, IWebHostEnvironment env)
+        {
+            bl = new BL(Util.ShareConnectionString(config, env));
+            _env = env;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var environment = _env.IsDevelopment() ? "Development" : "Production";
+            ViewBag.Environment = environment;
+            //var data = await bl.Getsystemproductbranddata(0, 1000);
+            return View();
+        }
+    }
+}
