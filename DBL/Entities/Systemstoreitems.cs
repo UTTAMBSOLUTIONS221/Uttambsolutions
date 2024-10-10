1c83db87b0528f6060dc2096350f85541150e2a3
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-10 20:43:34 +0300

    update

---
+++ DBL/Entities/Systemstoreitems.cs
@@ -0,0 +1,10 @@
+﻿namespace DBL.Entities
+{
+    public class Systemstoreitems
+    {
+        public int Storeitemid { get; set; }
+        public string? Storeitemname { get; set; }
+        public decimal Itembuyingprice { get; set; }
+        public decimal Itemsellingprice { get; set; }
+    }
+}
