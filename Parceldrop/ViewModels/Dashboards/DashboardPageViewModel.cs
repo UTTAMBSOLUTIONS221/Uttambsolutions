45a610b5673e759f2aefab4b111d22738f201204
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-12 12:55:37 +0300

    update

---
+++ Parceldrop/ViewModels/Dashboards/DashboardPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Parceldrop.Controls;
+using System.ComponentModel;
+
+namespace Parceldrop.ViewModels.Dashboards
+{
+    public partial class DashboardPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public DashboardPageViewModel()
+        {
+            AppShell.Current.FlyoutHeader = new FlyoutHeaderControl();
+        }
+    }
+}
