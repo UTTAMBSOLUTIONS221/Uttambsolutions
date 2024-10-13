e240828be6e0796d58fe963e020d4b1a6d5df656
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-13 10:06:32 +0300

    update

---
+++ Parceldrop/ViewModels/Parceldrop/ParcelDropViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DBL;
+using System.ComponentModel;
+
+namespace Parceldrop.ViewModels.Parceldrop
+{
+    public class ParcelDropViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly BL _bl;
+
+    }
+}
