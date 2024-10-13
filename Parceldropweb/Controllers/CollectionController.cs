55f1072b10ee5c0b523f4a4c2539932945470a3e
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-13 11:09:00 +0300

    update

---
+++ Parceldropweb/Controllers/CollectionController.cs
@@ -0,0 +1,29 @@
+﻿using DBL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Parceldropweb.Controllers
+{
+    [Authorize]
+    public class CollectionController : BaseController
+    {
+        private readonly BL bl;
+        private readonly IWebHostEnvironment _env;
+
+        public CollectionController(IConfiguration config, IWebHostEnvironment env)
+        {
+            bl = new BL(Util.ShareConnectionString(config, env));
+            _env = env;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Addcenter()
+        {
+            return PartialView();
+        }
+    }
+}
