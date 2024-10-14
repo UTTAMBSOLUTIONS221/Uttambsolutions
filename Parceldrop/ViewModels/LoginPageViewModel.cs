1a987d30d427da653bae7818ce2c5fb5aa606059
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-14 13:35:41 +0300

    update

--- Parceldrop/ViewModels/LoginPageViewModel.cs
+++ Parceldrop/ViewModels/LoginPageViewModel.cs
@@ -5,6 +5,7 @@ using DBL.Models;
 using Newtonsoft.Json;
 using Parceldrop.Constants;
 using Parceldrop.Views.Startup;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
@@ -14,11 +15,126 @@ namespace Parceldrop.ViewModels.Startup;
 public class LoginPageViewModel : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler PropertyChanged;
+    public string CopyrightText => $"© 2020 - {DateTime.Now.Year}  UTTAMB SOLUTIONS LIMITED";
+    public ICommand OpenPrivacyPolicyCommand => new Command<string>(async (url) =>
+    {
+        if (!string.IsNullOrEmpty(url))
+        {
+            await Browser.OpenAsync(url, BrowserLaunchMode.SystemPreferred);
+        }
+    });
+
     private readonly BL _bl;
+    private string _firstName;
+    private string _lastName;
+    private string _emailAddress;
+    private string _phoneNumber;
+    private string _staffDesignation;
+    private string _password;
+    private string _confirmPassword;
+    private bool _isProcessing;
+    private bool _accepttermsandcondition;
     private bool _isPasswordHidden;
     private string _passwordIconSource;
-    public string CopyrightText => $"© 2020 - {DateTime.Now.Year}  UTTAMB SOLUTIONS LIMITED";
+    private bool _isConfirmPasswordHidden;
+    private string _confirmPasswordIconSource;
 
+    public string FirstName
+    {
+        get => _firstName;
+        set
+        {
+            _firstName = value;
+            OnPropertyChanged();
+            ((Command)SignUpCommand).ChangeCanExecute();
+        }
+    }
+
+    public string LastName
+    {
+        get => _lastName;
+        set
+        {
+            _lastName = value;
+            OnPropertyChanged();
+            ((Command)SignUpCommand).ChangeCanExecute();
+        }
+    }
+
+    public string EmailAddress
+    {
+        get => _emailAddress;
+        set
+        {
+            _emailAddress = value;
+            OnPropertyChanged();
+            ((Command)SignUpCommand).ChangeCanExecute();
+        }
+    }
+
+    public string PhoneNumber
+    {
+        get => _phoneNumber;
+        set
+        {
+            _phoneNumber = value;
+            OnPropertyChanged();
+            ((Command)SignUpCommand).ChangeCanExecute();
+        }
+    }
+    public string StaffDesignation
+    {
+        get => _staffDesignation;
+        set
+        {
+            _staffDesignation = value;
+            OnPropertyChanged();
+            ((Command)SignUpCommand).ChangeCanExecute();
+        }
+    }
+    public bool Accepttermsandcondition
+    {
+        get => _accepttermsandcondition;
+        set
+        {
+            _accepttermsandcondition = value;
+            OnPropertyChanged();
+            ((Command)SignUpCommand).ChangeCanExecute();
+        }
+    }
+
+    public string Password
+    {
+        get => _password;
+        set
+        {
+            _password = value;
+            OnPropertyChanged();
+            ((Command)SignUpCommand).ChangeCanExecute();
+        }
+    }
+
+    public string ConfirmPassword
+    {
+        get => _confirmPassword;
+        set
+        {
+            _confirmPassword = value;
+            OnPropertyChanged();
+            ((Command)SignUpCommand).ChangeCanExecute();
+        }
+    }
+    private string _userName;
+    public string UserName
+    {
+        get => _userName;
+        set
+        {
+            _userName = value;
+            OnPropertyChanged();
+            ((Command)LoginCommand).ChangeCanExecute();
+        }
+    }
     public bool IsPasswordHidden
     {
         get => _isPasswordHidden;
@@ -37,6 +153,27 @@ public class LoginPageViewModel : INotifyPropertyChanged
         {
             _passwordIconSource = value;
             OnPropertyChanged(nameof(PasswordIconSource));
+        }
+    }
+
+    public bool IsConfirmPasswordHidden
+    {
+        get => _isConfirmPasswordHidden;
+        set
+        {
+            _isConfirmPasswordHidden = value;
+            OnPropertyChanged(nameof(IsConfirmPasswordHidden));
+            ConfirmPasswordIconSource = _isConfirmPasswordHidden ? "unvisible.png" : "visible.png";
+        }
+    }
+
+    public string ConfirmPasswordIconSource
+    {
+        get => _confirmPasswordIconSource;
+        set
+        {
+            _confirmPasswordIconSource = value;
+            OnPropertyChanged(nameof(ConfirmPasswordIconSource));
         }
     }
     private StaffDetailData _systemStaffTenantData;
@@ -74,45 +211,62 @@ public class LoginPageViewModel : INotifyPropertyChanged
     }
 
 
+    private ObservableCollection<ListModel> _systemstaffdesignation;
+    public ObservableCollection<ListModel> Systemstaffdesignation
+    {
+        get => _systemstaffdesignation;
+        set
+        {
+            _systemstaffdesignation = value;
+            OnPropertyChanged();
+        }
+    }
+    private ListModel _selectedStaffdesignation;
+    public ListModel SelectedStaffdesignation
+    {
+        get => _selectedStaffdesignation;
+        set
+        {
+            _selectedStaffdesignation = value;
+            if (value != null)
+            {
+                StaffDesignation = value.Value?.ToString();
+            }
+            OnPropertyChanged();
+        }
+    }
+    public ICommand TogglePasswordVisibilityCommand { get; }
+    public ICommand LoginCommand { get; }
+    public ICommand RegisterCommand { get; }
+    public ICommand ForgotPasswordCommand { get; }
+
+    public ICommand SignUpCommand => new Command(async () => await OnSignUp(), () => !IsProcessing);
+    public ICommand ToggleConfirmPasswordVisibilityCommand { get; }
     public LoginPageViewModel(BL bl)
     {
         _bl = bl;
         IsPasswordHidden = true; // Default to hidden password
         TogglePasswordVisibilityCommand = new Command(TogglePasswordVisibility);
+        IsPasswordHidden = true; // Default to hidden password
+        IsConfirmPasswordHidden = true; // Default to hidden password
+        ToggleConfirmPasswordVisibilityCommand = new Command(ToggleConfirmPasswordVisibility);
+        Systemstaffdesignation = new ObservableCollection<ListModel>
+        {
+            new ListModel { Value = "Owner", Text = "Owner" },
+            new ListModel { Value = "Agent", Text = "Agent" },
+            new ListModel { Value = "Tenant", Text = "Tenant" },
+        };
         LoginCommand = new Command(async () => await LoginAsync(), () => !IsProcessing);
         RegisterCommand = new Command(OnRegister);
         ForgotPasswordCommand = new Command(OnForgotPassword);
     }
-
+    private void ToggleConfirmPasswordVisibility()
+    {
+        IsConfirmPasswordHidden = !IsConfirmPasswordHidden;
+    }
     protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    private string _userName;
-    private string _password;
-    private bool _isProcessing;
-
-    public string UserName
-    {
-        get => _userName;
-        set
-        {
-            _userName = value;
-            OnPropertyChanged();
-            ((Command)LoginCommand).ChangeCanExecute();
-        }
-    }
-
-    public string Password
-    {
-        get => _password;
-        set
-        {
-            _password = value;
-            OnPropertyChanged();
-            ((Command)LoginCommand).ChangeCanExecute();
-        }
     }
 
     public bool IsProcessing
@@ -125,10 +279,6 @@ public class LoginPageViewModel : INotifyPropertyChanged
             ((Command)LoginCommand).ChangeCanExecute();
         }
     }
-    public ICommand TogglePasswordVisibilityCommand { get; }
-    public ICommand LoginCommand { get; }
-    public ICommand RegisterCommand { get; }
-    public ICommand ForgotPasswordCommand { get; }
 
 
     private string _systemStaffUserNameError;
@@ -239,6 +389,145 @@ public class LoginPageViewModel : INotifyPropertyChanged
         finally
         {
             IsProcessing = false;
+        }
+    }
+
+    private async Task OnSignUp()
+    {
+        IsProcessing = true;
+
+        if (!IsValidInput())
+        {
+            IsProcessing = false;
+            return;
+        }
+        try
+        {
+            IsProcessing = true;
+
+            var request = new SystemStaff
+            {
+                Firstname = FirstName,
+                Lastname = LastName,
+                Emailaddress = EmailAddress,
+                Phonenumber = PhoneNumber,
+                Designation = StaffDesignation,
+                Passwords = Password,
+                Confirmpasswords = ConfirmPassword,
+                Datecreated = DateTime.Now,
+                Datemodified = DateTime.Now,
+                Lastlogin = DateTime.Now,
+                Loginstatus = 0,
+                Accepttermsandcondition = Accepttermsandcondition,
+                Isactive = true,
+                Isdeleted = false,
+                Isdefault = false,
+                Updateprofile = true,
+                Parentid = 0,
+                Passwordresetdate = DateTime.Now.AddDays(90),
+            };
+
+            // Call your registration service here
+            var response = await _bl.Registersystemstaffdata(request);
+            if (response.RespStatus == 0 || response.RespStatus == 200)
+            {
+                FirstName = "";
+                LastName = "";
+                EmailAddress = "";
+                PhoneNumber = "";
+                StaffDesignation = "";
+                Password = "";
+                ConfirmPassword = "";
+                Accepttermsandcondition = false;
+                await Shell.Current.GoToAsync("//LoginPage");
+            }
+            else if (response.RespStatus == 1)
+            {
+                await Shell.Current.DisplayAlert("Warning", response.RespMessage, "OK");
+            }
+            else
+            {
+                await Shell.Current.DisplayAlert("Error", "Sever error occured. Kindly Contact Admin!", "OK");
+            }
+        }
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlert("Error", ex.Message, "OK");
+        }
+        finally
+        {
+            IsProcessing = false;
+        }
+    }
+
+    private string _systemStaffFirstNameError;
+    public string SystemStaffFirstNameError
+    {
+        get => _systemStaffFirstNameError;
+        set
+        {
+            _systemStaffFirstNameError = value;
+            OnPropertyChanged();
+        }
+    }
+    private string _systemStaffLastNameError;
+    public string SystemStaffLastNameError
+    {
+        get => _systemStaffLastNameError;
+        set
+        {
+            _systemStaffLastNameError = value;
+            OnPropertyChanged();
+        }
+    }
+    private string _systemStaffEmailAddressError;
+    public string SystemStaffEmailAddressError
+    {
+        get => _systemStaffEmailAddressError;
+        set
+        {
+            _systemStaffEmailAddressError = value;
+            OnPropertyChanged();
+        }
+    }
+    private string _systemStaffPhonenumberError;
+    public string SystemStaffPhonenumberError
+    {
+        get => _systemStaffPhonenumberError;
+        set
+        {
+            _systemStaffPhonenumberError = value;
+            OnPropertyChanged();
+        }
+    }
+    private string _systemStaffConfirmPasswordError;
+    public string SystemStaffConfirmPasswordError
+    {
+        get => _systemStaffConfirmPasswordError;
+        set
+        {
+            _systemStaffConfirmPasswordError = value;
+            OnPropertyChanged();
+        }
+    }
+    private string _systemStaffDesignationError;
+    public string SystemStaffDesignationError
+    {
+        get => _systemStaffDesignationError;
+        set
+        {
+            _systemStaffDesignationError = value;
+            OnPropertyChanged();
+        }
+    }
+    private string _systemStaffAccepttermsandconditionError;
+    public string SystemStaffAccepttermsandconditionError
+    {
+        get => _systemStaffAccepttermsandconditionError;
+        set
+        {
+            _systemStaffAccepttermsandconditionError = value;
+            OnPropertyChanged();
         }
     }
     private bool IsValidEmail(string email)
