9b37394a268780b5cfecc86b54c7419f6d22b416
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-14 20:05:58 +0300

    update

--- DBL/Entities/Parcelcollectioncenters.cs
+++ DBL/Entities/Parcelcollectioncenters.cs
@@ -5,9 +5,12 @@
         public int Collectioncenterid { get; set; }
         public string? Collectionname { get; set; }
         public int Countyid { get; set; }
+        public string? Countyname { get; set; }
         public int Subcountyid { get; set; }
+        public string? Subcountyname { get; set; }
         public int Subcountywardid { get; set; }
-        public string Streetorlandmark { get; set; }
+        public string? Subcountywardname { get; set; }
+        public string? Streetorlandmark { get; set; }
         public string? Phonenumber { get; set; }
         public string? Operatinghours { get; set; }
         public int Collectionstatus { get; set; }
