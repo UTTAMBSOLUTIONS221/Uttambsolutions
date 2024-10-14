34447461aad6e2b0a1eb1b4201a71d71b7e51048
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-14 07:59:26 +0300

    update

--- DBL/Entities/Collectioncenterparcels.cs
+++ DBL/Entities/Collectioncenterparcels.cs
@@ -4,13 +4,19 @@
     {
         public int Parcelid { get; set; }
         public int Senderid { get; set; }
+        public string? Sendername { get; set; }
         public int Receiverid { get; set; }
+        public string? Recievername { get; set; }
         public int Pickupcenterid { get; set; }
+        public string? Pickupcentername { get; set; }
         public int Deliverycenterid { get; set; }
+        public string? Deliverycentername { get; set; }
         public int Parceltypeid { get; set; }
+        public string? Parceltypename { get; set; }
         public decimal Parcelweight { get; set; }
         public string? Dimensions { get; set; }
         public int Parcelstatusid { get; set; }
+        public string? Parcelstatusname { get; set; }
         public int Transitdays { get; set; }
         public decimal Deliveryfee { get; set; }
         public string? Trackingnumber { get; set; }
