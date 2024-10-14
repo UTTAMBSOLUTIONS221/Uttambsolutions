c82cee83ba30505aaa6d5a01738547def3e1ec61
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-14 18:30:06 +0300

    update

--- Uttambsolutionsdatabase/dbo/Tables/Parcelcollectioncenters.sql
+++ Uttambsolutionsdatabase/dbo/Tables/Parcelcollectioncenters.sql
@@ -3,10 +3,15 @@
     [Collectionname]     VARCHAR (100) NOT NULL,
     [Collectionaddress]  VARCHAR (255) NOT NULL,
     [Phonenumber]        VARCHAR (15)  NULL,
+	[Countyid]                INT             NOT NULL,
+    [Subcountyid]             INT             NOT NULL,
+    [Subcountywardid]         INT             NOT NULL,
+    [Streetorlandmark]        VARCHAR (200)   NOT NULL,
+	[Maplatitude]             VARCHAR (200)   NULL,
+    [Maplongitude]            VARCHAR (200)   NULL,
     [Operatinghours]     VARCHAR (50)  NULL,
     [Collectionstatus]   INT           DEFAULT ((0)) NOT NULL,
     [Managerid]          BIGINT        NULL,
     PRIMARY KEY CLUSTERED ([Collectioncenterid] ASC),
     FOREIGN KEY ([Managerid]) REFERENCES [dbo].[Systemstaffs] ([Userid])
 );
-
