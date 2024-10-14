9e5388059e29b9a9383eafa319218a5d704306ce
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-14 13:27:20 +0300

    update

--- Parceldrop/Views/Startup/RegisterPage.xaml.cs
+++ Parceldrop/Views/Startup/RegisterPage.xaml.cs
@@ -1,9 +1,20 @@
+using Parceldrop.ViewModels.Startup;
+
 namespace Parceldrop.Views.Startup;
 
 public partial class RegisterPage : ContentPage
 {
-	public RegisterPage()
-	{
-		InitializeComponent();
-	}
+    public RegisterPage()
+    {
+        InitializeComponent();
+    }
+}
+
+public partial class RegisterPage : ContentPage
+{
+    public RegisterPage(LoginPageViewModel viewModel)
+    {
+        InitializeComponent();
+        this.BindingContext = viewModel;
+    }
 }
