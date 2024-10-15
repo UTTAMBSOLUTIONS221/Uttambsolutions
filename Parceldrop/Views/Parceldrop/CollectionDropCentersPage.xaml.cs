dc5489da910c22c90fcb5a463c921b914efb5ed8
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-15 08:11:11 +0300

    update

--- Parceldrop/Views/Parceldrop/CollectionDropCentersPage.xaml.cs
+++ Parceldrop/Views/Parceldrop/CollectionDropCentersPage.xaml.cs
@@ -1,9 +1,21 @@
+using Parceldrop.ViewModels.Parceldrop;
+
 namespace Parceldrop.Views.Parceldrop;
 
 public partial class CollectionDropCentersPage : ContentPage
 {
-	public CollectionDropCentersPage()
-	{
-		InitializeComponent();
-	}
+    private ParcelDropViewModel _viewModel;
+    public CollectionDropCentersPage(ParcelDropViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = _viewModel = viewModel;
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (_viewModel.LoadCurrentParcelDropCommand.CanExecute(null))
+        {
+            _viewModel.LoadCurrentParcelDropCommand.Execute(null);
+        }
+    }
 }
