01f038e143ea96a621c490fc642c4e64e7234ee8
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-16 19:55:52 +0300

    update

---
+++ DBL/Entities/Esaccosaccos.cs
@@ -0,0 +1,13 @@
+﻿namespace DBL.Entities
+{
+    public class Esaccosaccos
+    {
+        public int Saccoid { get; set; }
+        public string? Saccocode { get; set; }
+        public string? Sacconame { get; set; }
+        public string? Saccoemail { get; set; }
+        public string? Saccophone { get; set; }
+        public int Createdby { get; set; }
+        public DateTime Datecreated { get; set; }
+    }
+}
---
+++ DBL/Models/Saccosummarydatamodel.cs
@@ -0,0 +1,9 @@
+﻿using DBL.Entities;
+
+namespace DBL.Models
+{
+    public class Saccosummarydatamodel
+    {
+        public List<Esaccosaccos>? Esaccosaccosdata { get; set; }
+    }
+}
