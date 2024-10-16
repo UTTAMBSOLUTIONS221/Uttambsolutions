9ea712a4a8aeed3e8a4b9b0a42106fcb8c8094f2
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-16 16:53:42 +0300

    update

---
+++ Esacco/Constants/AppConstant.cs
@@ -0,0 +1,176 @@
+﻿using DBL;
+
+namespace Esacco.Constants
+{
+    public class AppConstant
+    {
+        public async static Task AddFlyoutMenusDetails()
+        {
+            AppShell.Current.FlyoutHeader = new FlyoutHeaderControl();
+            var adminDashboardInfo = AppShell.Current.Items.Where(f => f.Route == nameof(AdminDashboardPage)).FirstOrDefault();
+            if (adminDashboardInfo != null) AppShell.Current.Items.Remove(adminDashboardInfo);
+
+            var parcelCollectionCenterDashboardInfo = AppShell.Current.Items.Where(f => f.Route == nameof(ParcelCollectionCenterDashboardPage)).FirstOrDefault();
+            if (parcelCollectionCenterDashboardInfo != null) AppShell.Current.Items.Remove(parcelCollectionCenterDashboardInfo);
+
+            var parcelCollectionCourierDashboardInfo = AppShell.Current.Items.Where(f => f.Route == nameof(ParcelCollectionCourierDashboardPage)).FirstOrDefault();
+            if (parcelCollectionCourierDashboardInfo != null) AppShell.Current.Items.Remove(parcelCollectionCourierDashboardInfo);
+
+            var parcelCollectionCustomerDashboardInfo = AppShell.Current.Items.Where(f => f.Route == nameof(ParcelCollectionCustomerDashboardPage)).FirstOrDefault();
+            if (parcelCollectionCustomerDashboardInfo != null) AppShell.Current.Items.Remove(parcelCollectionCustomerDashboardInfo);
+
+            var bl = App.Current.Handler.MauiContext.Services.GetService<BL>();
+
+            if (App.UserDetails.Usermodel.Rolename == "Super Admin" || App.UserDetails.Usermodel.Rolename == "System Admin")
+            {
+                var flyoutItem = new FlyoutItem()
+                {
+                    Title = "Dashboard",
+                    Route = nameof(AdminDashboardPage),
+                    FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Icon = Icons.Dashboard,
+                            Title = "Dashboard",
+                            ContentTemplate = new DataTemplate(typeof(AdminDashboardPage)),
+                        },
+                    }
+                };
+                if (!AppShell.Current.Items.Contains(flyoutItem))
+                {
+                    AppShell.Current.Items.Add(flyoutItem);
+                    if (DeviceInfo.Platform == DevicePlatform.WinUI)
+                    {
+                        AppShell.Current.Dispatcher.Dispatch(async () =>
+                        {
+                            await Shell.Current.GoToAsync($"//{nameof(AdminDashboardPage)}");
+                        });
+                    }
+                    else
+                    {
+                        await Shell.Current.GoToAsync($"//{nameof(AdminDashboardPage)}");
+                    }
+                }
+            }
+            else if (App.UserDetails.Usermodel.Rolename == "Parcel Collection Center")
+            {
+                var flyoutItem = new FlyoutItem()
+                {
+                    Title = "Dashboard",
+                    Route = nameof(ParcelCollectionCenterDashboardPage),
+                    FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Icon = Icons.Dashboard,
+                            Title = "Dashboard",
+                            ContentTemplate = new DataTemplate(typeof(ParcelCollectionCenterDashboardPage)),
+                        },
+                        //new ShellContent
+                        //{
+                        //    Icon = Icons.user,
+                        //    Title = "Profile",
+                        //    ContentTemplate = new DataTemplate(() => new UserProfilePage(new UserProfilePageViewModel(bl))),
+                        //},
+                    }
+                };
+                if (!AppShell.Current.Items.Contains(flyoutItem))
+                {
+                    AppShell.Current.Items.Add(flyoutItem);
+                    if (DeviceInfo.Platform == DevicePlatform.WinUI)
+                    {
+                        AppShell.Current.Dispatcher.Dispatch(async () =>
+                        {
+                            await Shell.Current.GoToAsync($"//{nameof(ParcelCollectionCenterDashboardPage)}");
+                        });
+                    }
+                    else
+                    {
+                        await Shell.Current.GoToAsync($"//{nameof(ParcelCollectionCenterDashboardPage)}");
+                    }
+                }
+            }
+            else if (App.UserDetails.Usermodel.Rolename == "Parcel Drop Courier")
+            {
+                var flyoutItem = new FlyoutItem()
+                {
+                    Title = "Dashboard",
+                    Route = nameof(ParcelCollectionCourierDashboardPage),
+                    FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Icon = Icons.Dashboard,
+                            Title = "Dashboard",
+                            ContentTemplate = new DataTemplate(typeof(ParcelCollectionCourierDashboardPage)),
+                        },
+                        new ShellContent
+                        {
+                            Icon = Icons.user,
+                            Title = "Profile",
+                            ContentTemplate = new DataTemplate(() => new UserProfilePage(new LoginPageViewModel(bl))),
+                        },
+                        new ShellContent
+                        {
+                            Icon = Icons.user,
+                            Title = "Drop Centers",
+                            ContentTemplate = new DataTemplate(() => new CollectionDropCentersPage(new ParcelDropViewModel(bl))),
+                        },
+                    }
+                };
+                if (!AppShell.Current.Items.Contains(flyoutItem))
+                {
+                    AppShell.Current.Items.Add(flyoutItem);
+                    if (DeviceInfo.Platform == DevicePlatform.WinUI)
+                    {
+                        AppShell.Current.Dispatcher.Dispatch(async () =>
+                        {
+                            await Shell.Current.GoToAsync($"//{nameof(ParcelCollectionCourierDashboardPage)}");
+                        });
+                    }
+                    else
+                    {
+                        await Shell.Current.GoToAsync($"//{nameof(ParcelCollectionCourierDashboardPage)}");
+                    }
+                }
+            }
+            else
+            {
+                var flyoutItem = new FlyoutItem()
+                {
+                    Title = "Dashboard",
+                    Route = nameof(ParcelCollectionCustomerDashboardPage),
+                    FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems,
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Icon = Icons.Dashboard,
+                            Title = "Dashboard",
+                            ContentTemplate = new DataTemplate(typeof(ParcelCollectionCustomerDashboardPage)),
+                        },
+                    }
+                };
+                if (!AppShell.Current.Items.Contains(flyoutItem))
+                {
+                    AppShell.Current.Items.Add(flyoutItem);
+                    if (DeviceInfo.Platform == DevicePlatform.WinUI)
+                    {
+                        AppShell.Current.Dispatcher.Dispatch(async () =>
+                        {
+                            await Shell.Current.GoToAsync($"//{nameof(ParcelCollectionCustomerDashboardPage)}");
+                        });
+                    }
+                    else
+                    {
+                        await Shell.Current.GoToAsync($"//{nameof(ParcelCollectionCustomerDashboardPage)}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Esacco/Constants/Icons.cs
@@ -0,0 +1,20 @@
+﻿namespace Esacco.Constants
+{
+    public static class Icons
+    {
+        public const string Dashboard = "dashboard.png";
+        public const string user = "user.png";
+        public const string house = "house.png";
+        public const string reports = "reports.png";
+        public const string groupusers = "groupusers.png";
+        public const string dollar = "dollar.png";
+        public const string invoice = "invoice.png";
+        public const string agreement = "agreement.png";
+
+        public const string ContactUs = "contact_us.png";
+        public const string AboutUs = "about_us.png";
+        public const string People = "team.png";
+        public const string mysignature = "mysignature.jpg";
+
+    }
+}
