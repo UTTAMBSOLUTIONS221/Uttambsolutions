2e70c59481f4e28419f365aa25afee52f8c21766
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-16 20:59:55 +0300

    update

---
+++ Esacco/Helpers/CustomGridItemsLayout.cs
@@ -0,0 +1,33 @@
+﻿namespace Esacco.Helpers
+{
+    public class CustomGridItemsLayout : GridItemsLayout
+    {
+        public CustomGridItemsLayout() : base(1, ItemsLayoutOrientation.Vertical)
+        {
+        }
+
+        public void UpdateSpan(int itemCount)
+        {
+            if (itemCount == 0)
+            {
+                this.Span = 1;
+            }
+            else if (itemCount == 1)
+            {
+                this.Span = 1;
+            }
+            else if (itemCount == 2 || itemCount == 4)
+            {
+                this.Span = 2;
+            }
+            else if (itemCount == 3 || itemCount == 5)
+            {
+                this.Span = 3;
+            }
+            else
+            {
+                this.Span = 4; // Default span for more items
+            }
+        }
+    }
+}
