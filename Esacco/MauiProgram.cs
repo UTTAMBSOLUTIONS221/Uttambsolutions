122db340def9348a8850315a4cf60f598668114f
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-16 17:36:47 +0300

    update

--- Esacco/MauiProgram.cs
+++ Esacco/MauiProgram.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using DBL;
+using Esacco.ViewModels;
+using Microsoft.Extensions.Logging;
 
 namespace Esacco
 {
@@ -7,17 +10,35 @@ namespace Esacco
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
 
+            // Determine environment based on conditional compilation
+            string environment = "Production";
 #if DEBUG
-    		builder.Logging.AddDebug();
+            environment = "Development";
 #endif
+
+            // Use the environment to set up logging or other services
+            if (environment == "Development")
+            {
+                builder.Logging.AddDebug();
+            }
+
+            // Set up connection string or other services based on environment
+            string connectionString = environment == "Development" ? "Data Source=sql6032.site4now.net;Initial Catalog=db_aaa347_uttambsolutionsdev;user id=db_aaa347_uttambsolutionsdev_admin;password=Password123!;" : "Data Source=SQL6030.site4now.net;Initial Catalog=db_aaa347_uttambsolutions;user id=db_aaa347_uttambsolutions_admin;password=Password123!;";
+            builder.Services.AddSingleton<BL>(sp => new BL(connectionString));
+            builder
+              .UseMauiApp<App>().UseMauiCommunityToolkit()
+              .ConfigureFonts(fonts =>
+              {
+                  fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                  fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+              });
+
+
+            builder.Services.AddSingleton<LoginPage>();
+
+
+            builder.Services.AddSingleton<UserManagementViewModel>();
 
             return builder.Build();
         }
