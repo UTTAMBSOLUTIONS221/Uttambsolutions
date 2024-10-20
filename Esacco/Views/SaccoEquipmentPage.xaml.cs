35281a9a1ac2362034e7992d66e602f640fb27d3
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-10-20 10:49:47 +0300

    update

--- Esacco/Views/SaccoEquipmentPage.xaml.cs
+++ Esacco/Views/SaccoEquipmentPage.xaml.cs
@@ -1,9 +1,20 @@
-namespace Esacco.Views;
+using Esacco.ViewModels;
 
+namespace Esacco.Views;
 public partial class SaccoEquipmentPage : ContentPage
 {
-	public SaccoEquipmentPage()
-	{
-		InitializeComponent();
-	}
+    private EsaccoManagementViewModel _viewModel;
+    public SaccoEquipmentPage(EsaccoManagementViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = _viewModel = viewModel;
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (_viewModel.LoadSaccoSummaryDataCommand.CanExecute(null))
+        {
+            _viewModel.LoadSaccoSummaryDataCommand.Execute(null);
+        }
+    }
 }
